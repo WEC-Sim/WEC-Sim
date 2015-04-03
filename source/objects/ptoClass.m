f76eeb351504f1e24a553d930015fe9682864eef
Michael Lawson <lawsonro3@gmail.com>
2015-04-03 01:27:10 -0600

    WEC-Sim-1.1-alpha
    Former-commit-id: 2df134adcc9490d329c3bc48192d570b8735fc4e [formerly 2f1f08edd99adf0000d8b198921d0959e7dce8f8]
    Former-commit-id: 2221a42a4924b98d9475c31619f00d4d427e70aa

--- source/objects/ptoClass.m
+++ source/objects/ptoClass.m
@@ -1,4 +1,4 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Copyright 2014 the National Renewable Energy Laboratory and Sandia Corporation
 % 
 % Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,56 +12,56 @@
 % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 % See the License for the specific language governing permissions and
 % limitations under the License.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% This class defines the properities of PTO elements
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 classdef ptoClass<handle
-    properties
-        name                    = 'NOT DEFINED'                            % Name of the pto 
-        k                       = 0                                        % PTO stiffness. Default = 0
-        c                       = 0                                        % PTO damping. Default = 0
-        loc                     = [9999 9999 9999]                         % PTO location. Default = [0 0 0]        
+    properties                                                         
+        name                    = 'NOT DEFINED'                                 % Name of the pto 
+        k                       = 0                                             % PTO stiffness. Default = 0
+        c                       = 0                                             % PTO damping. Default = 0
+        loc                     = [999 999 999]                                 % PTO location. Default = [0 0 0]        
     end 
     
-    methods    
-        function obj = ptoClass(name) 
+    properties (SetAccess = 'public', GetAccess = 'public')%internal
+        ptoNum                  = []
+    end
+    
+    methods                                                            
+        function obj = ptoClass(name)                                  
         % Initilization function
-                 fprintf('Initializing the PTO Class... \n')
-                 obj.name = name;
+             obj.name = name;
         end
         
-        function obj = checkLoc(obj,action)
-        % Checks if location is set and outputs a warning or error.
+        function obj = checkLoc(obj,action)                            
         % Used in mask Initialization.
+        % Checks if location is set and outputs a warning or error.
             switch action
               case 'W'
-                % Because "Allow library block to modify its content"
-                % is selected in block's mask initialization, 
-                % this command runs twice, but warnings cannot be displayed 
-                % during the first initialization. 
-                if obj.loc == [9999 9999 9999]
-                    obj.loc = [8888 8888 8888];
-                elseif obj.loc == [8888 8888 8888]
+                if obj.loc == 999 % Because "Allow library block to modify its content" is selected in block's mask initialization, this command runs twice, but warnings cannot be displayed during the first initialization. 
+                    obj.loc = [888 888 888];
+                elseif obj.loc == 888
                     obj.loc = [0 0 0];
-                    s1= ['For ' obj.name ': pto.loc was changed '...
-                      'from [9999 9999 9999] to [0 0 0].'];
+                    s1= ['For ' obj.name ': pto.loc was changed from [999 999 999] to [0 0 0].'];
                     warning(s1)
                 end
               case 'E'
                   try
-                      if obj.loc == [9999 9999 9999]
-                        s1 = ['For ' obj.name ': pto(#).loc needs to'...
-                          ' be specified in the WEC-Sim input file.'...
-                          ' pto(#).loc is the [x y z] location, '...
-                          'in meters, for the rotational PTO.'];
+                      if obj.loc == 999
+                        s1 = ['For ' obj.name ': pto(#).loc needs to be specified in the WEC-Sim input file.'...
+                          ' pto.loc is the [x y z] location, in meters, for the rotational PTO.'];
                         error(s1)
                       end
                   catch exception
                       throwAsCaller(exception)
                   end
             end
+        end
+        
+        function listInfo(obj)                                         
+        % List PTO info
+            fprintf('\n\t***** PTO Name: %s *****\n',obj.name)
+            fprintf('\tPTO Stiffness           (N/m;Nm/rad) = %G\n',obj.k)
+            fprintf('\tPTO Damping           (Ns/m;Nsm/rad) = %G\n',obj.c)
         end
     end    
 end
--- source/wecSim.m
+++ source/wecSim.m
@@ -1,69 +1,152 @@
-%% WEC-Sim run file
-tic; clear; clc;close all; bdclose('all'); clearvars;
-
-%% Initiate the simulation class and specify input file name
-simu = simulationClass;
-
-%% Read input file
-waves.randPreDefined = 0; % Only used for irregular waves. Default is equal to 0; if it equals to 1, the waves pahse is pre-defined
-evalc([simu.inputFile]);
-
-% Start WEC-Sim log
-diary off; if exist('simulation.log','file'); delete('simulation.log'); end; 
+%%% WEC-Sim run file
+%% Start WEC-Sim log
+bdclose('all'); clc; diary off; 
+if exist('simulation.log','file'); delete('simulation.log'); end
 diary('simulation.log')
 
-%% Load SimMecahnics file and caluclate the number of bodies
-simu.loadSimMechModel(simu.simMechanicsFile);
-simu.numWecBodies = length(body(1,:));
-if exist('constraint') == 1; simu.numConstraints = length(constraint(1,:)); end
-if exist('pto')        == 1; simu.numPtos = length(pto(1,:));               end
 
-%% Set hydrodynamic data, body mass, and body cg for each body in the simulation
-for i = 1:simu.numWecBodies
-    if simu.hydroDataWamit == 0 % this is a hack to deal with simulations that use mutiple WAMIT runs with one body in each run
-       body(i).setHydroData(i,simu);
-    else
-       body(i).setHydroData(1,simu);
+%% Read input file
+fprintf('\nWEC-Sim Read Input File ...   \n'); tic; 
+evalc('wecSimInputFile');
+toc;
+
+%% Setup simulation
+fprintf('\nWEC-Sim Pre-processing ...   \n'); tic;
+simu.numWecBodies = length(body(1,:));
+if exist('constraint','var') == 1
+    simu.numConstraints = length(constraint(1,:));
+    for ii = 1:simu.numConstraints
+        constraint(ii).constraintNum = ii;
     end
-    body(i).setMass(simu);
-    body(i).setCg;
-    body(i).setMomOfInertia;
-    body(i).setGeom;
+end
+if exist('pto','var') == 1
+    simu.numPtos = length(pto(1,:));
+    for ii = 1:simu.numPtos
+        pto(ii).ptoNum = ii;
+    end
 end
 
-%% Check WAMIT data
-checkBem(body)
 
-%% Wave Setup
-waveSetup
+%% Check that the hydro data for each body is given for the same frequencies
+for ii = 1:simu.numWecBodies
+    if length(body(1).hydroData.simulation_parameters.w) ~= length(body(ii).hydroData.simulation_parameters.w)
+       error('BEM simulations for each body must have the same number of frequencies')
+    else
+       for jj = 1:length(body(1).hydroData.simulation_parameters.w)
+           if body(1).hydroData.simulation_parameters.w(jj) ~= body(ii).hydroData.simulation_parameters.w(jj)
+              error('BEM simulations must be run with the same frequencies.')
+           end; clear jj;
+       end
+    end
+end; clear ii; toc;
 
-%% HydroForce Pre-Processing
-hydroForcePre(body,waves,simu);
+
+%% HydroForces Pre-Processing: Wave Setup & HydroForcePre
+tic;
+fprintf('\nWEC-Sim Wave Setup & Model Setup & Run WEC-Sim ...   \n')
+simu.rhoDensitySetup(body(1).hydroData.simulation_parameters.rho,body(1).hydroData.simulation_parameters.g)
+waves.waveSetup(body(1).hydroData.simulation_parameters.w, body(1).hydroData.simulation_parameters.water_depth, simu.rampT, simu.dt, simu.maxIt, simu.g); 
+for kk = 1:simu.numWecBodies
+    body(kk).hydroForcePre(waves.w,simu.CIkt,waves.numFreq,simu.dt,simu.rho,waves.type,kk,simu.ssCalc);
+end; clear kk
+
 
 %% Output All the Simulation and Model Setting
-listSimulationParameters;
-listBodyInfo;
-listPtoConstraints;
+simu.listInfo(waves.typeNum);
 
-%% Run Simulation
-fprintf(['\nSimulating the WEC device defined in the SimMechanics '...
-   'model %s...   \n'],simu.simMechanicsFile)
-adjustMassMatrix(body);
-sim(simu.simMechanicsFile);
-restoreMassMatrix(body);
+waves.listInfo
+
+fprintf('\nList of Body: ');
+fprintf('Number of Bodies = %u \n',simu.numWecBodies)
+for i = 1:simu.numWecBodies
+    body(i).listInfo
+end;  clear i
+
+fprintf('\nList of PTO(s): ');
+if (exist('pto','var') == 0)
+    fprintf('No PTO in the system\n')
+else
+    fprintf('Number of PTOs = %G \n',length(pto(1,:)))
+    for i=1:simu.numPtos
+        pto(i).listInfo
+    end; clear i
+end
+
+fprintf('\nList of Constraint(s): ');
+if (exist('constraint','var') == 0)
+    fprintf('No Constraint in the system\n')
+else
+    fprintf('Number of Constraints = %G \n',length(constraint(1,:)))
+    for i=1:simu.numConstraints
+        constraint(i).listInfo
+    end; clear i
+end
 fprintf('\n')
 
-%% Post processing and Saving Results
-postResponse
+%% Load simMechanics file & Run Simulation
+fprintf('\nSimulating the WEC device defined in the SimMechanics model %s...   \n',simu.simMechanicsFile)
+simu.loadSimMechModel(simu.simMechanicsFile);
+for iBod = 1:simu.numWecBodies; body(iBod).adjustMassMatrix; end; clear iBod
+tDelayWarning = 'Simulink:blocks:TDelayTimeTooSmall';
+warning('off',tDelayWarning); clear tDelayWarning
+if simu.rampT == 0; simu.rampT = 10e-8; end
+if strcmp(simu.explorer,'on') &&  ~isfloat(waves.waterDepth)
+    waves.waterDepth = 200;
+    warning('Invalid water depth given. waves.waterDepth set to 200m for vizualisation.')
+end
+sim(simu.simMechanicsFile);
+if simu.rampT == 10e-8; simu.rampT = 0; end
 
-%% User Defined Post Processing Processing (e.g., calculate average power)
+for iBod = 1:simu.numWecBodies
+    body(iBod).restoreMassMatrix
+end; clear iBod
+
+
+%% %% Post processing and Saving Results
+% bodiesOutput = struct();
+for iBod = 1:simu.numWecBodies
+    eval(['body' num2str(iBod) '_out.name = body(' num2str(iBod) ').hydroData.properties.name;']);
+    if iBod == 1; bodiesOutput = body1_out; end
+    bodiesOutput(iBod) = eval(['body' num2str(iBod) '_out']);
+    eval(['clear body' num2str(iBod) '_out'])
+end; clear iBod
+if exist('pto','var')
+    for iPto = 1:simu.numPtos
+        eval(['pto' num2str(iPto) '_out.name = pto(' num2str(iPto) ').name;'])
+        if iPto == 1; ptosOutput = pto1_out; end
+        ptosOutput(iPto) = eval(['pto' num2str(iPto) '_out']);
+        eval(['clear pto' num2str(iPto) '_out'])
+    end; clear iPto
+else
+    ptosOutput = 0;
+end
+if exist('constraint','var')
+    for iCon = 1:simu.numConstraints
+        eval(['constraint' num2str(iCon) '_out.name = constraint(' num2str(iCon) ').name;'])
+        if iCon == 1; constraintsOutput = constraint1_out; end
+        constraintsOutput(iCon) = eval(['constraint' num2str(iCon) '_out']);
+        eval(['clear constraint' num2str(iCon) '_out'])
+    end; clear iCon
+else
+    constraintsOutput = 0;
+end
+output = responseClass(bodiesOutput,ptosOutput,constraintsOutput);
+clear bodiesOutput ptosOutput constraintsOutput
+
+for iBod = 1:simu.numWecBodies
+%     body(iBod).storeForceAddedMass(output.bodies(iBod).forceAddedMass)
+    output.bodies(iBod).forceTotal = output.bodies(iBod).forceTotal - output.bodies(iBod).forceAddedMass;
+    output.bodies(iBod).forceAddedMass = body(iBod).forceAddedMass(output.bodies(iBod).acceleration);
+    output.bodies(iBod).forceTotal = output.bodies(iBod).forceTotal + output.bodies(iBod).forceAddedMass;
+end; clear iBod
+
+% User Defined Post-Processing
 if exist('userDefinedFunctions.m','file') == 2
     userDefinedFunctions;                
 end
 
-clear ans
-toc
-diary off
+
+clear ans; toc; 
+diary off; movefile('simulation.log',simu.logFile)
 save(simu.caseFile)
-movefile('simulation.log',simu.logFile)
 
