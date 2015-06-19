a9778d560c35773661a8e7bda1ca445ae721bba3
Michael Lawson <lawsonro3@gmail.com>
2015-06-19 17:36:53 -0600

    updating applications to latest version of bemio

--- applications/OSWEC/hydroData/readWamit.py
+++ applications/OSWEC/hydroData/readWamit.py
@@ -1,23 +1,19 @@
+from bemio.io import wamit
+from bemio.io.output import write_hdf5
 
-from bemio.io import wamit as wio
-from bemio.data_structures import bem as hd
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.interactive(True)
 
 # Load the data
-w = wio.WamitOutput(out_file='oswec.out')
+w = wamit.WamitOutput(out_file='oswec.out')
 
 print w.data[0].num_bodies
 
 # Calculate IRF and plot
 for i in xrange(w.data[0].num_bodies):
-	w.data[i].calc_irf()
-#	w.data[i].calc_ss()
-
-# Plot hydrodynamic coefficients for the flap (body 0) only
-w.data[0].plot_irf([[0,0],[2,2]])
-w.data[0].plot_am_rd([[0,0],[2,2]])
+	w.data[i].calc_irf_excitation()
+#	w.data[i].calc_ss_excitation()
 
 # Save the data in HDF5 format
-hd.write_hdf5(w)
+write_hdf5(w)
