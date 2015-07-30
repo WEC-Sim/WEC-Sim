f8061ed293c8b86d29e832557f7d14554a886ba1
Michael Lawson <lawsonro3@gmail.com>
2015-07-30 16:41:35 -0600

    Several minor updates, no significant changes that effect the user significantly.
    * Move verification cases to the tutorials folder
    * Update to how hdf5 hydrodynamic files are read
    * Update to be compatible with latest version of bemio
    
    
    Former-commit-id: af6b07ce90eb6d49e6be6fef65a5be53474c335b [formerly d755e00c8231019d2bf66ade35be47b616058c58] [formerly e0d1a575356d20be64b118a833ed94f68c30decd]
    Former-commit-id: e7a92335e4f5bad4270440d84f6ca6229a48d2c8
    Former-commit-id: a8460647b937dee3afb55dfc72eeb9e59f396d15

--- source/objects/simulationClass.m
+++ source/objects/simulationClass.m
@@ -1,34 +1,34 @@
 % This class contains WEC-Sim simulation parameters and settings
 %
 % Copyright 2014 the National Renewable Energy Laboratory and Sandia Corporation
-% 
+%
 % Licensed under the Apache License, Version 2.0 (the "License");
 % you may not use this file except in compliance with the License.
 % You may obtain a copy of the License at
-% 
+%
 %     http://www.apache.org/licenses/LICENSE-2.0
-% 
+%
 % Unless required by applicable law or agreed to in writing, software
 % distributed und   er the License is distributed on an "AS IS" BASIS,
 % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 % See the License for the specific language governing permissions and
 % limitations under the License.
-classdef simulationClass<handle 
+classdef simulationClass<handle
     properties (Access = protected)
-        version             = 'Version 1.0'                                % WEC-Sim version                  
+        version             = 'Version 1.0'                                % WEC-Sim version
         outputDir           = 'output'                                     % Data output directory name
         time                = 0                                            % Simulation time [s] (default = 0 s)
     end
-    
+
     properties (Access = public)
-        numWecBodies        = [] % make these dependent variables          % Number of hydrodynamic bodies that comprise the WEC device (default = 'NOT DEFINED') 
-        numPtos             = []                                           % Number of power take-off elements in the model (default = 'NOT DEFINED') 
+        numWecBodies        = [] % make these dependent variables          % Number of hydrodynamic bodies that comprise the WEC device (default = 'NOT DEFINED')
+        numPtos             = []                                           % Number of power take-off elements in the model (default = 'NOT DEFINED')
         numConstraints      = []                                           % Number of contraints in the wec model (default = 'NOT DEFINED')
-        caseDir             = []                                           % WEC-Sim case directory (default = 'NOT DEFINED') 
+        caseDir             = []                                           % WEC-Sim case directory (default = 'NOT DEFINED')
         simMechanicsFile    = 'NOT DEFINED'                                % Simulink/SimMecahnics model file (default = 'NOT DEFINED')
         inputFile           = 'wecSimInputFile'                            % Name of WEC-Sim input file (default = 'wecSimInputFile')
         hydroDataWamit      = 0                                            % Equal to 1 if data from 1 WAMIT file, Equal to 0 if data from more than 1 input file (default = 0)
-        startTime           = 0                                            % Simulation start time (default = 0 s)  
+        startTime           = 0                                            % Simulation start time (default = 0 s)
         endTime             = 500                                          % Simulation end time (default = 500 s)
         dt                  = 0.1                                          % Simulation time step (default = 0.1 s)
         rampT               = 100                                          % Ramp time for wave forcing (default = 100 s)
@@ -39,28 +39,28 @@ classdef simulationClass<handle
 %        ssImport            = [];
 %        ssMax               = 10
 %        R2Thresh            = 0.95
-        mode                = 'normal'                                     %'normal','accelerator','rapid-accelerator' (default = 'normal') 
+        mode                = 'normal'                                     %'normal','accelerator','rapid-accelerator' (default = 'normal')
         solver              = 'ode4'                                       % PDE solver used by the Simulink/SimMechanics simulation (default = 'ode4')
         explorer            = 'on'                                         % SimMechanics Explorer 'on' or 'off' (default = 'on'
         numT                = []
         numDTPerT           = []
-        numRampT            = []        
+        numRampT            = []
         rho                 = 1000                                           % Density of water (default = 1000 kg/m^3)
         g                   = 9.81                                           % Acceleration due to gravity (default = 9.81 m/s)
         nlHydro             = 0                                            % Option for nonlinear hydrohanamics calculation: linear->'0', nonlinear->'1', (default = 0)
     end
-    
+
     properties (Dependent)
         maxIt                                                              % Total number of simulation time steps (default = dependent)        CIkt                                                               % Calculate the number of convolution integral timesteps (default = dependent)
-        CTTime                                                             % Convolution integral time series (default = dependent)       
+        CTTime                                                             % Convolution integral time series (default = dependent)
         logFile                                                            % File with run information summary
         caseFile                                                           % .mat file with all simulation information
         outputFileName
-        CIkt                                                               % Number of timesteps in the convolution integral length                                                             
+        CIkt                                                               % Number of timesteps in the convolution integral length
     end
-    
+
     methods
-     
+
         function obj = simulationClass(file)
              if nargin >= 1
                  obj.inputFile = file; % Function to change the name of the input file if desired (not reccomended)
@@ -73,11 +73,11 @@ classdef simulationClass<handle
              fprintf('Version: %s\n\n',obj.version)
              fprintf('Initializing the Simulation Class...\n')
 
-             obj.caseDir = pwd; cd(obj.caseDir);   
-             fprintf('\tCase Dir: %s \n',obj.caseDir)  
+             obj.caseDir = pwd; cd(obj.caseDir);
+             fprintf('\tCase Dir: %s \n',obj.caseDir)
 
              obj.time = [obj.startTime:obj.dt:obj.endTime];
-             
+
              obj.outputDir = ['.' filesep obj.outputDir];
 
              % Create the output directory. If the dir exists it will be
@@ -93,7 +93,7 @@ classdef simulationClass<handle
                 mkdir(obj.outputDir)
              end
         end
-        
+
         function obj = loadSimMechModel(obj,fName)
         % Loads the model and sets the simulation parameters
             load_system(fName);
@@ -106,66 +106,66 @@ classdef simulationClass<handle
                  'FixedStep',num2str(obj.dt),...
                  'SimMechanicsOpenEditorOnUpdate',obj.explorer);
         end
-        
-        function maxIt = get.maxIt(obj)                                    
+
+        function maxIt = get.maxIt(obj)
         % Calculates the number of time-steps
             maxIt = floor((obj.endTime - obj.startTime) / obj.dt);
         end
-        
+
         function CIkt = get.CIkt(obj)
-        % Function to calculate number of timesteps in the convolution integral length 
+        % Function to calculate number of timesteps in the convolution integral length
             CIkt = ceil(obj.CITime/obj.dt);
-        end        
+        end
 
         function CTTime = get.CTTime(obj)
         % Function to calculate the convolution integral time series
             CTTime = 0:obj.dt:obj.dt*obj.CIkt;
-        end  
-                
+        end
+
         function logFile = get.logFile(obj)
             if exist(obj.simMechanicsFile) ~=4
                 error('The simMecahnics file, %s, does not exist in the case directory',value)
             end
             logFile = [obj.caseDir filesep 'output' filesep obj.simMechanicsFile(1:end-4) '_simulationLog.txt'];
         end
-        
+
         function caseFile = get.caseFile(obj)
             if exist(obj.simMechanicsFile) ~=4
                 error('The simMecahnics file, %s, does not exist in the case directory',value)
             end
             caseFile = [obj.caseDir filesep 'output' filesep obj.simMechanicsFile(1:end-4) '_matlabWorkspace.mat'];
         end
-        
+
         function outputFileName = get.outputFileName(obj)
             if exist(obj.simMechanicsFile) ~=4
                 error('The simMecahnics file, %s, does not exist in the case directory',value)
             end
             outputFileName = [obj.caseDir filesep 'output' filesep obj.simMechanicsFile(1:end-4) '_output.mat'];
         end
-        
+
         function obj = set.simMechanicsFile(obj,value)
             if exist(value,'file') ~= 4
                 error('The simMecahnics file, %s, does not exist in the case directory',value)
             end
                 obj.simMechanicsFile = value;
         end
-        
+
         function rhoDensitySetup(obj,rho,g)
                 obj.rho = rho;
-                obj.g   = g;                
+                obj.g   = g;
         end
-        
+
         function listInfo(obj,waveTypeNum)
             fprintf('\nWEC-Sim Simulation Settings:\n');
             fprintf('\tTime Marching Solver                 = Fourth-Order Runge-Kutta Formula \n')
-            fprintf('\tStart Time                     (sec) = %G\n',obj.startTime) 
-            fprintf('\tEnd Time                       (sec) = %G\n',obj.endTime) 
+            fprintf('\tStart Time                     (sec) = %G\n',obj.startTime)
+            fprintf('\tEnd Time                       (sec) = %G\n',obj.endTime)
             fprintf('\tTime Step Size                 (sec) = %G\n',obj.dt)
-            fprintf('\tRamp Function Time             (sec) = %G\n',obj.rampT) 
+            fprintf('\tRamp Function Time             (sec) = %G\n',obj.rampT)
             if waveTypeNum > 10
-                fprintf('\tConvolution Integral Interval  (sec) = %G\n',obj.CITime) 
+                fprintf('\tConvolution Integral Interval  (sec) = %G\n',obj.CITime)
             end
-            fprintf('\tTotal Number of Time Step            = %u \n',obj.maxIt) 
+            fprintf('\tTotal Number of Time Step            = %u \n',obj.maxIt)
         end
     end
 end
