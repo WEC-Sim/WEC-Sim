7502750b493fb72e8388a9c8fbbf4c8227ffbb77
Michael Lawson <lawsonro3@gmail.com>
2015-07-30 16:41:35 -0600

    Several minor updates, no significant changes that effect the user significantly.
    * Move verification cases to the tutorials folder
    * Update to how hdf5 hydrodynamic files are read
    * Update to be compatible with latest version of bemio
    
    
    Former-commit-id: cc1c202d3c6e55c8b67754346ccb9ee0855d67d4 [formerly 2bd8d51ccff7a3bfec81f9b28a0074a0218a98bb] [formerly 8777cc37037db1a28e99be208fc4e3f9379e02af]
    Former-commit-id: ffab3b48260a98ea91e5b31436da4e12e8ebbbca
    Former-commit-id: 1b2165b7ca58651cdb12d342e43c0cd432ed43e3

--- source/wecSimStartup.m
+++ source/wecSimStartup.m
@@ -2,11 +2,12 @@
 % Copy the code in this file and place it in you 'startup.m' file
 
 % Set the wecSimPath variable to the location of the WEC-Sim source directory
-wecSimPath = <WEC-SIM PATH>; 
+wecSimPath = <WEC-SIM PATH>;
+wecSimInit;
 
 % Add the source folder to the MATLAB path
 folderToAdd = 'source';
 addpath(genpath([wecSimPath filesep folderToAdd]));
 
 % Clear unneeded variables
-clear wecSimPath folderToAdd
+clear folderToAdd
