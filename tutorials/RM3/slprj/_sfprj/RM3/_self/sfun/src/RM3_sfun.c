475f91338ad6050214765a5cd8c2834d92a32d82
ratanakso <sor@onid.oregonstate.edu>
2015-09-02 14:35:53 -0600

    Added PTO-Sim documentation

---
+++ tutorials/RM3/slprj/_sfprj/RM3/_self/sfun/src/RM3_sfun.c
@@ -0,0 +1,309 @@
+/* Include files */
+
+#include "RM3_sfun.h"
+#include "RM3_sfun_debug_macros.h"
+
+/* Type Definitions */
+
+/* Named Constants */
+
+/* Variable Declarations */
+
+/* Variable Definitions */
+uint32_T _RM3MachineNumber_;
+
+/* Function Declarations */
+
+/* Function Definitions */
+void RM3_initializer(void)
+{
+}
+
+void RM3_terminator(void)
+{
+}
+
+/* SFunction Glue Code */
+unsigned int sf_RM3_method_dispatcher(SimStruct *simstructPtr, unsigned int
+  chartFileNumber, const char* specsCksum, int_T method, void *data)
+{
+  return 0;
+}
+
+extern void sf_RM3_uses_exported_functions(int nlhs, mxArray * plhs[], int nrhs,
+  const mxArray * prhs[])
+{
+  plhs[0] = mxCreateLogicalScalar(0);
+}
+
+unsigned int sf_RM3_process_check_sum_call( int nlhs, mxArray * plhs[], int nrhs,
+  const mxArray * prhs[] )
+{
+
+#ifdef MATLAB_MEX_FILE
+
+  char commandName[20];
+  if (nrhs<1 || !mxIsChar(prhs[0]) )
+    return 0;
+
+  /* Possible call to get the checksum */
+  mxGetString(prhs[0], commandName,sizeof(commandName)/sizeof(char));
+  commandName[(sizeof(commandName)/sizeof(char)-1)] = '\0';
+  if (strcmp(commandName,"sf_get_check_sum"))
+    return 0;
+  plhs[0] = mxCreateDoubleMatrix( 1,4,mxREAL);
+  if (nrhs>1 && mxIsChar(prhs[1])) {
+    mxGetString(prhs[1], commandName,sizeof(commandName)/sizeof(char));
+    commandName[(sizeof(commandName)/sizeof(char)-1)] = '\0';
+    if (!strcmp(commandName,"machine")) {
+      ((real_T *)mxGetPr((plhs[0])))[0] = (real_T)(720997089U);
+      ((real_T *)mxGetPr((plhs[0])))[1] = (real_T)(2192419422U);
+      ((real_T *)mxGetPr((plhs[0])))[2] = (real_T)(932807828U);
+      ((real_T *)mxGetPr((plhs[0])))[3] = (real_T)(581223356U);
+    } else if (!strcmp(commandName,"exportedFcn")) {
+      ((real_T *)mxGetPr((plhs[0])))[0] = (real_T)(0U);
+      ((real_T *)mxGetPr((plhs[0])))[1] = (real_T)(0U);
+      ((real_T *)mxGetPr((plhs[0])))[2] = (real_T)(0U);
+      ((real_T *)mxGetPr((plhs[0])))[3] = (real_T)(0U);
+    } else if (!strcmp(commandName,"makefile")) {
+      ((real_T *)mxGetPr((plhs[0])))[0] = (real_T)(458901757U);
+      ((real_T *)mxGetPr((plhs[0])))[1] = (real_T)(2635835376U);
+      ((real_T *)mxGetPr((plhs[0])))[2] = (real_T)(333400997U);
+      ((real_T *)mxGetPr((plhs[0])))[3] = (real_T)(423017009U);
+    } else if (nrhs==3 && !strcmp(commandName,"chart")) {
+      unsigned int chartFileNumber;
+      chartFileNumber = (unsigned int)mxGetScalar(prhs[2]);
+      switch (chartFileNumber) {
+       default:
+        ((real_T *)mxGetPr((plhs[0])))[0] = (real_T)(0.0);
+        ((real_T *)mxGetPr((plhs[0])))[1] = (real_T)(0.0);
+        ((real_T *)mxGetPr((plhs[0])))[2] = (real_T)(0.0);
+        ((real_T *)mxGetPr((plhs[0])))[3] = (real_T)(0.0);
+      }
+    } else if (!strcmp(commandName,"target")) {
+      ((real_T *)mxGetPr((plhs[0])))[0] = (real_T)(3061339410U);
+      ((real_T *)mxGetPr((plhs[0])))[1] = (real_T)(1991824845U);
+      ((real_T *)mxGetPr((plhs[0])))[2] = (real_T)(3599338742U);
+      ((real_T *)mxGetPr((plhs[0])))[3] = (real_T)(2357874978U);
+    } else {
+      return 0;
+    }
+  } else {
+    ((real_T *)mxGetPr((plhs[0])))[0] = (real_T)(4055981118U);
+    ((real_T *)mxGetPr((plhs[0])))[1] = (real_T)(1167545403U);
+    ((real_T *)mxGetPr((plhs[0])))[2] = (real_T)(1874246340U);
+    ((real_T *)mxGetPr((plhs[0])))[3] = (real_T)(4268548442U);
+  }
+
+  return 1;
+
+#else
+
+  return 0;
+
+#endif
+
+}
+
+unsigned int sf_RM3_autoinheritance_info( int nlhs, mxArray * plhs[], int nrhs,
+  const mxArray * prhs[] )
+{
+
+#ifdef MATLAB_MEX_FILE
+
+  char commandName[32];
+  char aiChksum[64];
+  if (nrhs<3 || !mxIsChar(prhs[0]) )
+    return 0;
+
+  /* Possible call to get the autoinheritance_info */
+  mxGetString(prhs[0], commandName,sizeof(commandName)/sizeof(char));
+  commandName[(sizeof(commandName)/sizeof(char)-1)] = '\0';
+  if (strcmp(commandName,"get_autoinheritance_info"))
+    return 0;
+  mxGetString(prhs[2], aiChksum,sizeof(aiChksum)/sizeof(char));
+  aiChksum[(sizeof(aiChksum)/sizeof(char)-1)] = '\0';
+
+  {
+    unsigned int chartFileNumber;
+    chartFileNumber = (unsigned int)mxGetScalar(prhs[1]);
+    switch (chartFileNumber) {
+     default:
+      plhs[0] = mxCreateDoubleMatrix(0,0,mxREAL);
+    }
+  }
+
+  return 1;
+
+#else
+
+  return 0;
+
+#endif
+
+}
+
+unsigned int sf_RM3_get_eml_resolved_functions_info( int nlhs, mxArray * plhs[],
+  int nrhs, const mxArray * prhs[] )
+{
+
+#ifdef MATLAB_MEX_FILE
+
+  char commandName[64];
+  if (nrhs<2 || !mxIsChar(prhs[0]))
+    return 0;
+
+  /* Possible call to get the get_eml_resolved_functions_info */
+  mxGetString(prhs[0], commandName,sizeof(commandName)/sizeof(char));
+  commandName[(sizeof(commandName)/sizeof(char)-1)] = '\0';
+  if (strcmp(commandName,"get_eml_resolved_functions_info"))
+    return 0;
+
+  {
+    unsigned int chartFileNumber;
+    chartFileNumber = (unsigned int)mxGetScalar(prhs[1]);
+    switch (chartFileNumber) {
+     default:
+      plhs[0] = mxCreateDoubleMatrix(0,0,mxREAL);
+    }
+  }
+
+  return 1;
+
+#else
+
+  return 0;
+
+#endif
+
+}
+
+unsigned int sf_RM3_third_party_uses_info( int nlhs, mxArray * plhs[], int nrhs,
+  const mxArray * prhs[] )
+{
+  char commandName[64];
+  char tpChksum[64];
+  if (nrhs<3 || !mxIsChar(prhs[0]))
+    return 0;
+
+  /* Possible call to get the third_party_uses_info */
+  mxGetString(prhs[0], commandName,sizeof(commandName)/sizeof(char));
+  commandName[(sizeof(commandName)/sizeof(char)-1)] = '\0';
+  mxGetString(prhs[2], tpChksum,sizeof(tpChksum)/sizeof(char));
+  tpChksum[(sizeof(tpChksum)/sizeof(char)-1)] = '\0';
+  if (strcmp(commandName,"get_third_party_uses_info"))
+    return 0;
+
+  {
+    unsigned int chartFileNumber;
+    chartFileNumber = (unsigned int)mxGetScalar(prhs[1]);
+    switch (chartFileNumber) {
+     default:
+      plhs[0] = mxCreateDoubleMatrix(0,0,mxREAL);
+    }
+  }
+
+  return 1;
+}
+
+unsigned int sf_RM3_jit_fallback_info( int nlhs, mxArray * plhs[], int nrhs,
+  const mxArray * prhs[] )
+{
+  char commandName[64];
+  char tpChksum[64];
+  if (nrhs<3 || !mxIsChar(prhs[0]))
+    return 0;
+
+  /* Possible call to get the jit_fallback_info */
+  mxGetString(prhs[0], commandName,sizeof(commandName)/sizeof(char));
+  commandName[(sizeof(commandName)/sizeof(char)-1)] = '\0';
+  mxGetString(prhs[2], tpChksum,sizeof(tpChksum)/sizeof(char));
+  tpChksum[(sizeof(tpChksum)/sizeof(char)-1)] = '\0';
+  if (strcmp(commandName,"get_jit_fallback_info"))
+    return 0;
+
+  {
+    unsigned int chartFileNumber;
+    chartFileNumber = (unsigned int)mxGetScalar(prhs[1]);
+    switch (chartFileNumber) {
+     default:
+      plhs[0] = mxCreateDoubleMatrix(0,0,mxREAL);
+    }
+  }
+
+  return 1;
+}
+
+unsigned int sf_RM3_updateBuildInfo_args_info( int nlhs, mxArray * plhs[], int
+  nrhs, const mxArray * prhs[] )
+{
+  char commandName[64];
+  char tpChksum[64];
+  if (nrhs<3 || !mxIsChar(prhs[0]))
+    return 0;
+
+  /* Possible call to get the updateBuildInfo_args_info */
+  mxGetString(prhs[0], commandName,sizeof(commandName)/sizeof(char));
+  commandName[(sizeof(commandName)/sizeof(char)-1)] = '\0';
+  mxGetString(prhs[2], tpChksum,sizeof(tpChksum)/sizeof(char));
+  tpChksum[(sizeof(tpChksum)/sizeof(char)-1)] = '\0';
+  if (strcmp(commandName,"get_updateBuildInfo_args_info"))
+    return 0;
+
+  {
+    unsigned int chartFileNumber;
+    chartFileNumber = (unsigned int)mxGetScalar(prhs[1]);
+    switch (chartFileNumber) {
+     default:
+      plhs[0] = mxCreateDoubleMatrix(0,0,mxREAL);
+    }
+  }
+
+  return 1;
+}
+
+void sf_RM3_get_post_codegen_info( int nlhs, mxArray * plhs[], int nrhs, const
+  mxArray * prhs[] )
+{
+  unsigned int chartFileNumber = (unsigned int) mxGetScalar(prhs[0]);
+  char tpChksum[64];
+  mxGetString(prhs[1], tpChksum,sizeof(tpChksum)/sizeof(char));
+  tpChksum[(sizeof(tpChksum)/sizeof(char)-1)] = '\0';
+  switch (chartFileNumber) {
+   default:
+    break;
+  }
+
+  plhs[0] = mxCreateDoubleMatrix(0,0,mxREAL);
+}
+
+void RM3_debug_initialize(struct SfDebugInstanceStruct* debugInstance)
+{
+  _RM3MachineNumber_ = sf_debug_initialize_machine(debugInstance,"RM3","sfun",0,
+    0,0,0,0);
+  sf_debug_set_machine_event_thresholds(debugInstance,_RM3MachineNumber_,0,0);
+  sf_debug_set_machine_data_thresholds(debugInstance,_RM3MachineNumber_,0);
+}
+
+void RM3_register_exported_symbols(SimStruct* S)
+{
+}
+
+static mxArray* sRtwOptimizationInfoStruct= NULL;
+mxArray* load_RM3_optimization_info(void)
+{
+  if (sRtwOptimizationInfoStruct==NULL) {
+    sRtwOptimizationInfoStruct = sf_load_rtw_optimization_info("RM3", "RM3");
+    mexMakeArrayPersistent(sRtwOptimizationInfoStruct);
+  }
+
+  return(sRtwOptimizationInfoStruct);
+}
+
+void unload_RM3_optimization_info(void)
+{
+  if (sRtwOptimizationInfoStruct!=NULL) {
+    mxDestroyArray(sRtwOptimizationInfoStruct);
+    sRtwOptimizationInfoStruct = NULL;
+  }
+}
