a19ee4b222de928cbc6611211d82bd71eeb83f01
Carlos A Michelen Strofer <c.michelen.strofer@gmail.com>
2015-09-17 15:51:48 -0600

    moved the getWecSimVer function to the simulation class
    Former-commit-id: fe57dfbf7bbca800788abbe59b877e8aa51eb5e0
    Former-commit-id: 1f90fe8de4c12733f057bd73a27c1b0dff7f71b7

--- source/functions/getWecSimVer.m
+++
@@ -1,6 +0,0 @@
-function git_ver = getWecSimVer
-
-ws_exe = which('wecSim');
-ws_dir = fileparts(ws_exe);
-git_ver_file = [ws_dir '/../.git/refs/heads/master'];
-git_ver = textread(git_ver_file,'%s');
