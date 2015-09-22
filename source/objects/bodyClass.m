b1b317c65381b053fab1d993153c2b61cf61258d
cmichelen <cmichel@sandia.gov>
2015-09-21 18:14:45 -0600

    visualization using Paraview
    Former-commit-id: f76c4816057bc5102a0b7dd0636d97cde0f00db9
    Former-commit-id: 7f9e75c50136629fdcdea14a31314d9054753c91

--- source/objects/bodyClass.m
+++ source/objects/bodyClass.m
@@ -33,6 +33,8 @@ classdef bodyClass<handle
                                     'initAngularDispAngle', 0)                  % Initial displacement of cog - Angle of rotation - used for decay tests (format: [radians], default = 0)
         linearDamping     = [0 0 0 0 0 0]                                       % Linear drag coefficient, vector length 6
         userDefinedExcIRF = []                                                  % Excitation IRF from BEMIO used for User-Defined Time-Series
+        viz               = struct('color', [1 1 0], ...                        % Visualization color for either SimMechanics Explorer or Paraview.
+                                    'opacity', 1)                               % Visualization opacity for either SimMechanics Explorer or Paraview.
     end
 
     properties (SetAccess = 'public', GetAccess = 'public') %body geometry stl file
@@ -386,5 +388,130 @@ classdef bodyClass<handle
             end
             clear tmp
         end
+
+        function xn = rotateXYZ(obj,x,ax,t)
+            % function to rotate a point about an arbitrary axis
+            % x: 3-componenet coordiantes
+            % ax: axis about which to rotate (must be a normal vector)
+            % t: rotation angle
+            % xn: new coordinates after rotation
+            rotMat = zeros(3);
+            rotMat(1,1) = ax(1)*ax(1)*(1-cos(t))    + cos(t);
+            rotMat(1,2) = ax(2)*ax(1)*(1-cos(t))    + ax(3)*sin(t);
+            rotMat(1,3) = ax(3)*ax(1)*(1-cos(t))    - ax(2)*sin(t);
+            rotMat(2,1) = ax(1)*ax(2)*(1-cos(t))    - ax(3)*sin(t);
+            rotMat(2,2) = ax(2)*ax(2)*(1-cos(t))    + cos(t);
+            rotMat(2,3) = ax(3)*ax(2)*(1-cos(t))    + ax(1)*sin(t);
+            rotMat(3,1) = ax(1)*ax(3)*(1-cos(t))    + ax(2)*sin(t);
+            rotMat(3,2) = ax(2)*ax(3)*(1-cos(t))    - ax(1)*sin(t);
+            rotMat(3,3) = ax(3)*ax(3)*(1-cos(t))    + cos(t);
+            xn = x*rotMat;
+        end
+
+        function verts_out = offsetXYZ(obj,verts,x)
+            % Function to move the position vertices
+            verts_out(:,1) = verts(:,1) + x(1);
+            verts_out(:,2) = verts(:,2) + x(2);
+            verts_out(:,3) = verts(:,3) + x(3);
+        end
+
+        function write_paraview_vtp(obj, t, pos_all, bodyname, model, simdate,cellareas,hspressure,wavenonlinearpressure,wavelinearpressure)
+            numVertex = obj.bodyGeometry.numVertex;
+            numFace = obj.bodyGeometry.numFace;
+            vertex = obj.bodyGeometry.vertex;
+            face = obj.bodyGeometry.face;
+            for it = 1:length(t)
+                % calculate new position
+                pos = pos_all(it,:);
+                vertex_mod = obj.rotateXYZ(vertex,[1 0 0],pos(4));
+                vertex_mod = obj.rotateXYZ(vertex_mod,[0 1 0],pos(5));
+                vertex_mod = obj.rotateXYZ(vertex_mod,[0 0 1],pos(6));
+                vertex_mod = obj.offsetXYZ(vertex_mod,pos(1:3));
+                % open file
+                filename = ['vtk' filesep 'body' num2str(obj.bodyNumber) '_' bodyname filesep bodyname '_' num2str(it) '.vtp'];
+                fid = fopen(filename, 'w');
+                % write header
+                fprintf(fid, '<?xml version="1.0"?>\n');
+                fprintf(fid, ['<!-- WEC-Sim Visualization using ParaView -->\n']);
+                fprintf(fid, ['<!--   model: ' model ' - ran on ' simdate ' -->\n']);
+                fprintf(fid, ['<!--   body:  ' bodyname ' -->\n']);
+                fprintf(fid, ['<!--   time:  ' num2str(t(it)) ' -->\n']);
+                fprintf(fid, '<VTKFile type="PolyData" version="0.1">\n');
+                fprintf(fid, '  <PolyData>\n');
+                % write body info
+                fprintf(fid,['    <Piece NumberOfPoints="' num2str(numVertex) '" NumberOfPolys="' num2str(numFace) '">\n']);
+                % write points
+                fprintf(fid,'      <Points>\n');
+                fprintf(fid,'        <DataArray type="Float32" NumberOfComponents="3" format="ascii">\n');
+                for ii = 1:numVertex    
+                    fprintf(fid, '          %5.5f %5.5f %5.5f\n', vertex_mod(ii,:));
+                end; 
+                clear vertex_mod
+                fprintf(fid,'        </DataArray>\n');
+                fprintf(fid,'      </Points>\n');
+                % write tirangles connectivity
+                fprintf(fid,'      <Polys>\n');
+                fprintf(fid,'        <DataArray type="Int32" Name="connectivity" format="ascii">\n');
+                for ii = 1:numFace
+                    fprintf(fid, '          %i %i %i\n', face(ii,:)-1);
+                end; 
+                fprintf(fid,'        </DataArray>\n');
+                fprintf(fid,'        <DataArray type="Int32" Name="offsets" format="ascii">\n');
+                fprintf(fid, '         ');
+                for ii = 1:numFace
+                    n = ii * 3;
+                    fprintf(fid, ' %i', n);
+                end;
+                fprintf(fid, '\n');
+                fprintf(fid,'        </DataArray>\n');
+                fprintf(fid, '      </Polys>\n');
+                % write cell data
+                fprintf(fid,'      <CellData>\n');
+                  % Cell Areas
+                if ~isempty(cellareas)
+                    fprintf(fid,'        <DataArray type="Float32" Name="Cell Area" NumberOfComponents="1" format="ascii">\n');
+                    for ii = 1:numFace
+                        fprintf(fid, '          %i', cellareas(it,ii));
+                    end; 
+                    fprintf(fid, '\n');
+                    fprintf(fid,'        </DataArray>\n');
+                end
+                  % Hydrostatic Pressure
+                if ~isempty(hspressure)
+                    fprintf(fid,'        <DataArray type="Float32" Name="Hydrostatic Pressure" NumberOfComponents="1" format="ascii">\n');
+                    for ii = 1:numFace
+                        fprintf(fid, '          %i', hspressure(it,ii));
+                    end; 
+                    fprintf(fid, '\n');
+                    fprintf(fid,'        </DataArray>\n');
+                end
+                  % Non-Linear Froude-Krylov Wave Pressure
+                if ~isempty(wavenonlinearpressure)
+                    fprintf(fid,'        <DataArray type="Float32" Name="Wave Pressure NonLinear" NumberOfComponents="1" format="ascii">\n');
+                    for ii = 1:numFace
+                        fprintf(fid, '          %i', wavenonlinearpressure(it,ii));
+                    end; 
+                    fprintf(fid, '\n');
+                    fprintf(fid,'        </DataArray>\n');
+                end
+                  % Linear Froude-Krylov Wave Pressure
+                if ~isempty(wavelinearpressure)
+                    fprintf(fid,'        <DataArray type="Float32" Name="Wave Pressure Linear" NumberOfComponents="1" format="ascii">\n');
+                    for ii = 1:numFace
+                        fprintf(fid, '          %i', wavelinearpressure(it,ii));
+                    end; 
+                    fprintf(fid, '\n');
+                    fprintf(fid,'        </DataArray>\n');
+                end
+                fprintf(fid,'      </CellData>\n');
+                % end file
+                fprintf(fid, '    </Piece>\n');
+                fprintf(fid, '  </PolyData>\n');
+                fprintf(fid, '</VTKFile>');
+                % close file
+                fclose(fid);
+            end
+        end
+
     end
 end
