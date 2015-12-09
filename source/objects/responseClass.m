2e0337e063b5eccabb12f57267a1e9b712f684df
Carlos A Michelen Strofer <c.michelen.strofer@gmail.com>
2015-12-09 10:56:33 -0700

    Update responseClass.m
    Former-commit-id: a0fe9b3ba07bed272692079364d89d2dfd3bac09
    Former-commit-id: f98f3d5478ec35665a03251798ef807fc83c0263
    Former-commit-id: 1d82bcdcb1dd5e37eb97f2ba1c020e34ce4e0626

--- source/objects/responseClass.m
+++ source/objects/responseClass.m
@@ -133,14 +133,14 @@ classdef responseClass<handle
             fprintf(fid,['  <!-- Wave:  ' wavetype ' -->\n']);
             for jj = 1:length(t)
                  fprintf(fid, ['    <DataSet timestep="' num2str(t(jj)) '" group="" part="" \n']);
-                 fprintf(fid, ['             file="waves' filesep 'waves_' num2str(jj) '.vtp"/>\n']);
+                 fprintf(fid, ['             file="waves' '/' 'waves_' num2str(jj) '.vtp"/>\n']);
             end 
             % write bodies
             for ii = 1:length(bodies)
                 fprintf(fid,['  <!-- Body' num2str(ii) ':  ' bodies{ii} ' -->\n']);
                 for jj = 1:length(t)
                      fprintf(fid, ['    <DataSet timestep="' num2str(t(jj)) '" group="" part="" \n']);
-                     fprintf(fid, ['             file="body' num2str(ii) '_' bodies{ii} filesep bodies{ii} '_' num2str(jj) '.vtp"/>\n']);
+                     fprintf(fid, ['             file="body' num2str(ii) '_' bodies{ii} '/' bodies{ii} '_' num2str(jj) '.vtp"/>\n']);
                 end
             end
             % close file
