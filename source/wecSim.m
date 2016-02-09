c2390247f0d21844bf1c598be3c59457ab17bd27
Yi-Hsiang <yi-hsiang.yu@nrel.gov>
2016-02-09 14:55:11 -0700

    update
    clear a variable
    
    
    Former-commit-id: 7c4ae0534a2ac8247bd3e64642c0ec11800dce28
    Former-commit-id: 1916db9a453184ee7d09340d726e1ea7b7c5da15

--- source/wecSim.m
+++ source/wecSim.m
@@ -216,7 +216,7 @@ simu.loadSimMechModel(simu.simMechanicsFile);
 sim(simu.simMechanicsFile);
 % Restore modified stuff
 clear nlHydro sv_linearHydro sv_nonlinearHydro ssCalc radiation_option sv_convolution sv_stateSpace sv_constantCoeff typeNum B2B sv_B2B sv_noB2B;
-clear nhbod* sv_b* sv_noWave sv_regularWaves sv_irregularWaves sv_udfWaves sv_meanFS sv_MEOn sv_MEOff morrisonElement;
+clear nhbod* sv_b* sv_noWave sv_regularWaves sv_irregularWaves sv_udfWaves sv_instFS sv_meanFS sv_MEOn sv_MEOff morrisonElement;
 toc
 
 
