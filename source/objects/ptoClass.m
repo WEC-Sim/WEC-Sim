154c86c48b66ca86dd0d8f7129deb29c6ec17118
Carlos A Michelen Strofer <c.michelen.strofer@gmail.com>
2016-02-11 15:32:32 -0700

    comments, doc
    Former-commit-id: 0282f575988aa38d882ce1e52203d226f7d93ad1
    Former-commit-id: 47dd950cface6aa70a2702d51930ae2c588f54b2
    Former-commit-id: 7b3232134b77efecfbfa013d5d21e35d83d76ec3

--- source/objects/ptoClass.m
+++ source/objects/ptoClass.m
@@ -20,10 +20,11 @@ classdef ptoClass<handle
         k                       = 0                                             % PTO stiffness. Default = 0
         c                       = 0                                             % PTO damping. Default = 0
         loc                     = [999 999 999]                                 % PTO location. Default = [0 0 0]        
-        orientation             = struct('z', [0, 0, 1], ...                    % Vector defining the direction of the Z-coordinate for the PTO.
-                                         'y', [0, 1, 0], ...                    % Vector defining the direction of the Y-coordinate for the PTO.
-                                         'x', [], ...                           % Internally calculated vector defining the direction of the X-coordinate for the PTO.
-                                         'rotationMatrix',[])                   % Internally calculated rotation matrix to go form standard coordinate orientation to the PTO's coordinate orientation.
+        orientation             = struct(...                                    % Structure defining orientation parameters
+                                         'z', [0, 0, 1], ...                        % Vector defining the direction of the Z-coordinate for the PTO.
+                                         'y', [0, 1, 0], ...                        % Vector defining the direction of the Y-coordinate for the PTO.
+                                         'x', [], ...                               % Internally calculated vector defining the direction of the X-coordinate for the PTO.
+                                         'rotationMatrix',[])                       % Internally calculated rotation matrix to go form standard coordinate orientation to the PTO's coordinate orientation.
     end 
     
     properties (SetAccess = 'public', GetAccess = 'public')%internal
@@ -37,8 +38,8 @@ classdef ptoClass<handle
         end
         
         function obj = checkLoc(obj,action)                            
-            % Used in mask Initialization.
             % Checks if location is set and outputs a warning or error.
+            % Used in mask Initialization.
             switch action
               case 'W'
                 if obj.loc == 999 % Because "Allow library block to modify its content" is selected in block's mask initialization, this command runs twice, but warnings cannot be displayed during the first initialization. 
@@ -62,6 +63,7 @@ classdef ptoClass<handle
         end
         
         function obj = setOrientation(obj)
+            % Sets orientation based on user input
             obj.orientation.z = obj.orientation.z / norm(obj.orientation.z);
             obj.orientation.y = obj.orientation.y / norm(obj.orientation.y);
             z = obj.orientation.z;
@@ -75,7 +77,7 @@ classdef ptoClass<handle
         end
 
         function setInitLoc(obj, loc_at_rest, x_rot, ax_rot, ang_rot, addLinDisp)
-            % function to set the initial location when having initial displacement
+            % Function to set the initial location when having initial displacement
             % loc_at_rest: location at rest 
             % x_rot: rotation point
             % ax_rot: axis about which to rotate (must be a normal vector)
@@ -89,7 +91,7 @@ classdef ptoClass<handle
         end
 
         function xn = rotateXYZ(obj,x,ax,t)
-            % function to rotate a point about an arbitrary axis
+            % Function to rotate a point about an arbitrary axis
             % x: 3-componenet coordiantes
             % ax: axis about which to rotate (must be a normal vector)
             % t: rotation angle
