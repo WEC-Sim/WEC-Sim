2dd3467ca410fb6b6719a86bc47f8519e59f8690
j-vanrij <jennifer.vanrij@nrel.gov>
2016-08-11 16:57:12 -0600

    MATLAB BEMIO
    The python based BEMIO functions have been converted to MATLAB. Example
    usage is provided in the WEC-Sim tutorial hydroData files.
    
    
    Former-commit-id: bd35edd3617a73e00156a3f8a74c98b655abfec1
    Former-commit-id: 67c493439744bffdb356e1b4750df4b9a380952a

---
+++ source/functions/BEMIO/Plot_BEMIO.m
@@ -0,0 +1,224 @@
+function Plot_BEMIO(hydro)
+
+p = waitbar(0,'Plotting BEMIO results…');  % Progress bar
+
+%% Added Mass
+clear X Y Legends
+Fig1 = figure('Position',[50,500,975,521]);
+Title = ['Normalized Added Mass: $$\bar{A}_{i,j}(\omega) = {\frac{A_{i,j}(\omega)}{\rho}}$$'];
+Subtitles = {'Surge','Heave','Pitch'};
+XLables = {'$$\omega (rad/s)$$','$$\omega (rad/s)$$','$$\omega (rad/s)$$'};
+YLables = {'$$\bar{A}_{1,1}(\omega)$$','$$\bar{A}_{3,3}(\omega)$$','$$\bar{A}_{5,5}(\omega)$$'};
+X = hydro.w;
+for i = 1:hydro.Nb
+    Y(1,i,:) = squeeze(hydro.A(6*i-5,6*i-5,:));
+    Legends{1,i} = [hydro.body{i}];
+    Y(2,i,:) = squeeze(hydro.A(6*i-3,6*i-3,:));
+    Legends{2,i} = [hydro.body{i}];
+    Y(3,i,:) = squeeze(hydro.A(6*i-1,6*i-1,:));
+    Legends{3,i} = [hydro.body{i}];
+end
+Notes = {'Notes:',...
+    ['$$\bullet$$ $$\bar{A}_{i,j}(\omega)$$ should tend towards a constant, ',...
+    '$$A_{\infty}$$, within the specified $$\omega$$ range.'],...
+    ['$$\bullet$$ Only $$\bar{A}_{i,j}(\omega)$$ for the surge, heave, and ',...
+    'pitch DOFs are plotted here. If another DOF is significant to the system, ',...
+    'that $$\bar{A}_{i,j}(\omega)$$ should also be plotted and verified before ',...
+    'proceeding.']};
+FormatPlot(Fig1,Title,Subtitles,XLables,YLables,X,Y,Legends,Notes)
+waitbar(1/6);
+
+%% Radiation Damping
+clear X Y Legends
+Fig2 = figure('Position',[50,300,975,521]);
+Title = ['Normalized Radiation Damping: $$\bar{B}_{i,j}(\omega) = {\frac{B_{i,j}(\omega)}{\rho\omega}}$$'];
+Subtitles = {'Surge','Heave','Pitch'};
+XLables = {'$$\omega (rad/s)$$','$$\omega (rad/s)$$','$$\omega (rad/s)$$'};
+YLables = {'$$\bar{B}_{1,1}(\omega)$$','$$\bar{B}_{3,3}(\omega)$$','$$\bar{B}_{5,5}(\omega)$$'};
+X = hydro.w;
+for i = 1:hydro.Nb
+    Y(1,i,:) = squeeze(hydro.B(6*i-5,6*i-5,:));
+    Legends{1,i} = [hydro.body{i}];
+    Y(2,i,:) = squeeze(hydro.B(6*i-3,6*i-3,:));
+    Legends{2,i} = [hydro.body{i}];
+    Y(3,i,:) = squeeze(hydro.B(6*i-1,6*i-1,:));
+    Legends{3,i} = [hydro.body{i}];
+end
+Notes = {'Notes:',...
+    ['$$\bullet$$ $$\bar{B}_{i,j}(\omega)$$ should tend towards zero within ',...
+    'the specified $$\omega$$ range.'],...
+    ['$$\bullet$$ Only $$\bar{B}_{i,j}(\omega)$$ for the surge, heave, and ',...
+    'pitch DOFs are plotted here. If another DOF is significant to the system ',...
+    'that $$\bar{B}_{i,j}(\omega)$$ should also be plotted and verified before ',...
+    'proceeding.']};
+FormatPlot(Fig2,Title,Subtitles,XLables,YLables,X,Y,Legends,Notes)
+waitbar(2/6);
+
+%% Radiation IRFs
+clear X Y Legends
+Fig3 = figure('Position',[50,100,975,521]);
+Title = ['Normalized Radiation Impulse Response Functions: ',...
+    '$$\bar{K}_{i,j}(t) = {\frac{2}{\pi}}\int_0^{\infty}{\frac{B_{i,j}(\omega)}{\rho}}\cos({\omega}t)d\omega$$'];
+Subtitles = {'Surge','Heave','Pitch'};
+XLables = {'$$t (s)$$','$$t (s)$$','$$t (s)$$'};
+YLables = {'$$\bar{K}_{1,1}(t)$$','$$\bar{K}_{3,3}(t)$$','$$\bar{K}_{3,3}(t)$$'};
+X = hydro.ra_t;
+n = 1;
+for i = 1:hydro.Nb
+    Y(1,n,:) = squeeze(hydro.ra_K(6*i-5,6*i-5,:));
+    Legends{1,n} = [hydro.body{i}];
+    Y(2,n,:) = squeeze(hydro.ra_K(6*i-3,6*i-3,:));
+    Legends{2,n} = [hydro.body{i}];
+    Y(3,n,:) = squeeze(hydro.ra_K(6*i-1,6*i-1,:));
+    Legends{3,n} = [hydro.body{i}];
+    if isfield(hydro,'ss_A')==1
+        n = n+1;
+        Y(1,n,:) = squeeze(hydro.ss_K(6*i-5,6*i-5,:));
+        Legends{1,n} = [hydro.body{i},' (SS)'];
+        Y(2,n,:) = squeeze(hydro.ss_K(6*i-3,6*i-3,:));
+        Legends{2,n} = [hydro.body{i},' (SS)'];
+        Y(3,n,:) = squeeze(hydro.ss_K(6*i-1,6*i-1,:));
+        Legends{3,n} = [hydro.body{i},' (SS)'];
+    end
+    n = n+1;
+end
+Notes = {'Notes:',...
+    ['$$\bullet$$ The IRF should tend towards zero within the specified timeframe. ',...
+    'If it does not, attempt to correct this by adjusting the $$\omega$$ and ',...
+    '$$t$$ range and/or step size used in the IRF calculation.'],...
+    ['$$\bullet$$ Only the IRFs for the surge, heave, and pitch DOFs are plotted ',...
+    'here. If another DOF is significant to the system, that IRF should also ',...
+    'be plotted and verified before proceeding.']};
+FormatPlot(Fig3,Title,Subtitles,XLables,YLables,X,Y,Legends,Notes)
+waitbar(3/6);
+
+%% Excitation Force Magnitude
+B=1;  % Wave heading index
+clear X Y Legends
+Fig4 = figure('Position',[950,500,975,521]);
+Title = ['Normalized Excitation Force Magnitude: ',...
+    '$$\bar{X_i}(\omega,\beta) = {\frac{X_i(\omega,\beta)}{{\rho}g}}$$'];
+Subtitles = {'Surge','Heave','Pitch'};
+XLables = {'$$\omega (rad/s)$$','$$\omega (rad/s)$$','$$\omega (rad/s)$$'};
+YLables = {['$$\bar{X_1}(\omega,\beta$$',' = ',num2str(hydro.beta(B)),'$$^{\circ}$$)'],...
+    ['$$\bar{X_3}(\omega,\beta$$',' = ',num2str(hydro.beta(B)),'$$^{\circ}$$)'],...
+    ['$$\bar{X_5}(\omega,\beta$$',' = ',num2str(hydro.beta(B)),'$$^{\circ}$$)']};
+X = hydro.w;
+for i = 1:hydro.Nb
+    Y(1,i,:) = squeeze(hydro.ex_ma(6*i-5,B,:));
+    Legends{1,i} = [hydro.body{i}];
+    Y(2,i,:) = squeeze(hydro.ex_ma(6*i-3,B,:));
+    Legends{2,i} = [hydro.body{i}];
+    Y(3,i,:) = squeeze(hydro.ex_ma(6*i-1,B,:));
+    Legends{3,i} = [hydro.body{i}];
+end
+Notes = {''};
+FormatPlot(Fig4,Title,Subtitles,XLables,YLables,X,Y,Legends,Notes)
+waitbar(4/6);
+
+%% Excitation Force Phase
+B=1;  % Wave heading index
+clear X Y Legends
+Fig5 = figure('Position',[950,300,975,521]);
+Title = ['Excitation Force Phase: $$\phi_i(\omega,\beta)$$'];
+Subtitles = {'Surge','Heave','Pitch'};
+XLables = {'$$\omega (rad/s)$$','$$\omega (rad/s)$$','$$\omega (rad/s)$$'};
+YLables = {['$$\phi_1(\omega,\beta$$',' = ',num2str(hydro.beta(B)),'$$^{\circ})$$'],...
+    ['$$\phi_3(\omega,\beta$$',' = ',num2str(hydro.beta(B)),'$$^{\circ}$$)'],...
+    ['$$\phi_5(\omega,\beta$$',' = ',num2str(hydro.beta(B)),'$$^{\circ}$$)']};
+X = hydro.w;
+for i = 1:hydro.Nb
+    Y(1,i,:) = squeeze(hydro.ex_ph(6*i-5,B,:));
+    Legends{1,i} = [hydro.body{i}];
+    Y(2,i,:) = squeeze(hydro.ex_ph(6*i-3,B,:));
+    Legends{2,i} = [hydro.body{i}];
+    Y(3,i,:) = squeeze(hydro.ex_ph(6*i-1,B,:));
+    Legends{3,i} = [hydro.body{i}];
+end
+Notes = {''};
+FormatPlot(Fig5,Title,Subtitles,XLables,YLables,X,Y,Legends,Notes)
+waitbar(5/6);
+
+%% Excitation IRFs
+B=1;  % Wave heading index
+clear X Y Legends
+Fig6 = figure('Position',[950,100,975,521]);
+Title = ['Normalized Excitation Impulse Response Functions:   ',...
+    '$$\bar{K}_i(t) = {\frac{1}{2\pi}}\int_{-\infty}^{\infty}{\frac{X_i(\omega,\beta)e^{i{\omega}t}}{{\rho}g}}d\omega$$'];
+Subtitles = {'Surge','Heave','Pitch'};
+XLables = {'$$t (s)$$','$$t (s)$$','$$t (s)$$'};
+YLables = {['$$\bar{K}_1(t,\beta$$',' = ',num2str(hydro.beta(B)),'$$^{\circ}$$)'],...
+    ['$$\bar{K}_3(t,\beta$$',' = ',num2str(hydro.beta(B)),'$$^{\circ}$$)'],...
+    ['$$\bar{K}_5(t,\beta$$',' = ',num2str(hydro.beta(B)),'$$^{\circ}$$)']};
+X = hydro.ex_t;
+for i = 1:hydro.Nb
+    Y(1,i,:) = squeeze(hydro.ex_K(6*i-5,B,:));
+    Legends{1,i} = [hydro.body{i}];
+    Y(2,i,:) = squeeze(hydro.ex_K(6*i-3,B,:));
+    Legends{2,i} = [hydro.body{i}];
+    Y(3,i,:) = squeeze(hydro.ex_K(6*i-1,B,:));
+    Legends{3,i} = [hydro.body{i}];
+end
+Notes = {'Notes:',...
+    ['$$\bullet$$ The IRF should tend towards zero within the specified timeframe. ',...
+    'If it does not, attempt to correct this by adjusting the $$\omega$$ and ',...
+    '$$t$$ range and/or step size used in the IRF calculation.'],...
+    ['$$\bullet$$ Only the IRFs for the first wave heading, surge, heave, and ',...
+    'pitch DOFs are plotted here. If another wave heading or DOF is significant ',...
+    'to the system, that IRF should also be plotted and verified before proceeding.']};
+FormatPlot(Fig6,Title,Subtitles,XLables,YLables,X,Y,Legends,Notes)
+waitbar(6/6);
+
+close(p);
+end
+
+
+%% Format
+function FormatPlot(fig,heading,subtitle,x_lables,y_lables,X_data,Y_data,legends,notes)
+
+axes1 = axes('Parent',fig,'Position',[0.0731 0.3645 0.2521 0.4720]);
+hold(axes1,'on');
+box(axes1,'on');
+title(subtitle(1));
+xlabel(x_lables(1),'Interpreter','latex');
+ylabel(y_lables(1),'Interpreter','latex');
+
+axes2 = axes('Parent',fig,'Position',[0.3983 0.3645 0.2521 0.4720]);
+hold(axes2,'on');
+box(axes2,'on');
+title(subtitle(2));
+xlabel(x_lables(2),'Interpreter','latex');
+ylabel(y_lables(2),'Interpreter','latex');
+
+axes3 = axes('Parent',fig,'Position',[0.7235 0.3645 0.2521 0.4720]);
+hold(axes3,'on');
+box(axes3,'on');
+title(subtitle(3));
+xlabel(x_lables(3),'Interpreter','latex');
+ylabel(y_lables(3),'Interpreter','latex');
+
+annotation(fig,'textbox',[0.0 0.9 1.0 0.1],...
+    'String',heading,...
+    'Interpreter','latex',...
+    'HorizontalAlignment','center',...
+    'FitBoxToText','off',...
+    'FontWeight','bold',...
+    'FontSize',12,...
+    'EdgeColor','none');
+annotation(fig,'textbox',[0.0 0.0 1.0 0.2628],...
+    'String',notes,...
+    'Interpreter','latex',...
+    'FitBoxToText','off',...
+    'EdgeColor','none');
+
+[p,b,s]=size(Y_data);
+for i = 1:b
+    plot(X_data,squeeze(Y_data(1,i,:)),'LineWidth',1,'Parent',axes1);
+    plot(X_data,squeeze(Y_data(2,i,:)),'LineWidth',1,'Parent',axes2);
+    plot(X_data,squeeze(Y_data(3,i,:)),'LineWidth',1,'Parent',axes3);
+end
+legend(axes1,legends(1,:),'location','best','Box','off','Interpreter','none')
+legend(axes2,legends(2,:),'location','best','Box','off','Interpreter','none')
+legend(axes3,legends(3,:),'location','best','Box','off','Interpreter','none')
+
+end
