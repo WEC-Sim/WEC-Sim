f2fce67ccd96c17c21a6941ace3efb7c419bcf99
j-vanrij <jennifer.vanrij@nrel.gov>
2016-09-12 12:55:52 -0600

    Update RM3 tutorial with improved BEM (WAMIT) data
    Update RM3 tutorial with improved BEM (WAMIT) data
    
    
    Former-commit-id: 438d292b80d827de550a39d53c49af65dee7084d
    Former-commit-id: f37b811beb2c338633c6a7a2060afe38b9699dd5

--- tutorials/RM3/hydroData/bemio.m
+++ tutorials/RM3/hydroData/bemio.m
@@ -2,8 +2,8 @@ clc; clear all; close all;
 hydro = struct();
 
 hydro = Read_WAMIT(hydro,'rm3.out',[]);
-hydro = Radiation_IRF(hydro,100,501,501,[],1.5);
+hydro = Radiation_IRF(hydro,60,[],[],[],[]);
 hydro = Radiation_IRF_SS(hydro,[],[]);
-hydro = Excitation_IRF(hydro,100,501,501,[],[]);
+hydro = Excitation_IRF(hydro,157,[],[],[],[]);
 Write_H5(hydro)
 Plot_BEMIO(hydro)
