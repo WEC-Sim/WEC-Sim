10c7c9911fb34a145d78c91263c0f2adb0b59812
j-vanrij <jennifer.vanrij@nrel.gov>
2016-09-12 12:55:52 -0600

    Update RM3 tutorial with improved BEM (WAMIT) data
    Update RM3 tutorial with improved BEM (WAMIT) data

--- tutorials/RM3/hydroData/readWamit.py
+++ tutorials/RM3/hydroData/readWamit.py
@@ -8,9 +8,9 @@ num_bodies = wamit_data.body[0].num_bodies
 
 # Calculate IRF
 for i in xrange(num_bodies):
-	wamit_data.body[i].calc_irf_radiation(t_end=100., n_t=501, n_w=501, w_max=1.5)
+	wamit_data.body[i].calc_irf_radiation(t_end=60.)
 	wamit_data.body[i].calc_ss_radiation()
-	wamit_data.body[i].calc_irf_excitation(t_end=100., n_t=501, n_w=501)
+	wamit_data.body[i].calc_irf_excitation(t_end=157.)
 
 # Save the data in HDF5 format
 write_hdf5(wamit_data)
