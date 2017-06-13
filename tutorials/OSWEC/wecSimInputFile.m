a4cc95bb8b264af10bd59af1312f10df686f28fd
Kelley Ruehl <kmruehl@sandia.gov>
2017-06-13 07:53:53 -0600

    update submodule and input files
    Former-commit-id: 0fe888940b09620343fe22f320e3856b97c7e5cf

--- tutorials/OSWEC/wecSimInputFile.m
+++ tutorials/OSWEC/wecSimInputFile.m
@@ -1,29 +1,29 @@
 %% Simulation Data
-simu = simulationClass();
-simu.simMechanicsFile = 'OSWEC.slx';          % Specify Simulink Model File
-% simu.mode = 'normal';                       % Specify Simulation Mode ('normal','accelerator','rapid-accelerator')
-% simu.explorer='on';                         % Turn SimMechanics Explorer (on/off)
-% simu.startTime = 0;                         % Simulation Start Time [s]
-simu.endTime=400;
-simu.solver = 'ode4';                         %simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
-simu.dt = 0.1; 							      %Simulation time-step [s] for a convolution function in the radiation force calculation 
-simu.rampT = 100;
-simu.CITime = 30;
+simu = simulationClass();               % Initialize Simulation Class
+simu.simMechanicsFile = 'OSWEC.slx';    % Specify Simulink Model File
+simu.mode = 'normal';                   % Specify Simulation Mode ('normal','accelerator','rapid-accelerator')
+simu.explorer='on';                     % Turn SimMechanics Explorer (on/off)
+simu.startTime = 0;                     % Simulation Start Time [s]
+simu.endTime=400;                       % Simulation End Time [s]        
+simu.solver = 'ode4';                   % simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
+simu.dt = 0.01;                         % Simulation Time-Step [s]
+simu.rampT = 100;                       % Wave Ramp Time Length [s]
+simu.CITime = 30;                       % Specify CI Time [s]
 
 %% Wave Information
 %% noWaveCIC, no waves with radiation CIC  
-% waves = waveClass('noWaveCIC');       %Create the Wave Variable and Specify Type  
+% waves = waveClass('noWaveCIC');       % Initialize Wave Class and Specify Type  
 
 %% Regular Waves 
-waves = waveClass('regular');
-waves.H = 2.5;
-waves.T = 8;
+waves = waveClass('regular');           % Initialize Wave Class and Specify Type                                 
+waves.H = 2.5;                          % Wave Height [m]
+waves.T = 8;                            % Wave Period [s]
 
 %% Irregular Waves using PM Spectrum with Convolution Integral Calculation
-% waves = waveClass('irregular');
-% waves.H = 2.5;
-% waves.T = 8;
-% waves.spectrumType = 'PM';
+% waves = waveClass('irregular');         % Initialize Wave Class and Specify Type
+% waves.H = 2.5;                          % Significant Wave Height [m]
+% waves.T = 8;                            % Peak Period [s]
+% waves.spectrumType = 'PM';              % Specify Spectrum Type
 
 %% Irregular Waves using User-Defined Spectrum
 % waves = waveClass('irregularImport');
@@ -31,23 +31,23 @@ waves.T = 8;
 
 %% Body Data
 %% Flap
-body(1) = bodyClass('hydroData/oswec.h5');   % Initialize bodyClass for Flap
-body(1).mass = 127000;                         % User-Defined mass [kg]
-body(1).momOfInertia = [1.85e6 1.85e6 1.85e6]; % Moment of Inertia [kg-m^2]
-body(1).geometryFile = 'geometry/flap.stl';    % Geometry File
+body(1) = bodyClass('hydroData/oswec.h5');      % Initialize bodyClass for Flap
+body(1).mass = 127000;                          % User-Defined mass [kg]
+body(1).momOfInertia = [1.85e6 1.85e6 1.85e6];  % Moment of Inertia [kg-m^2]
+body(1).geometryFile = 'geometry/flap.stl';     % Geometry File
 
 %% Base
-body(2) = bodyClass('hydroData/oswec.h5');   % Initialize bodyClass for Base
-body(2).geometryFile = 'geometry/base.stl';    % Geometry File
-body(2).mass = 'fixed';                        % Creates Fixed Body
+body(2) = bodyClass('hydroData/oswec.h5');      % Initialize bodyClass for Base
+body(2).geometryFile = 'geometry/base.stl';     % Geometry File
+body(2).mass = 'fixed';                         % Creates Fixed Body
 
 %% PTO and Constraint Parameters
 %% Fixed
-constraint(1)= constraintClass('Constraint1'); % Initialize ConstraintClass for Constraint1
-constraint(1).loc = [0 0 -10];
+constraint(1)= constraintClass('Constraint1');  % Initialize ConstraintClass for Constraint1
+constraint(1).loc = [0 0 -10];                  % Constraint Location [m]
 
 %% Rotational PTO
-pto(1) = ptoClass('PTO1');                     % Initialize ptoClass for PTO1
-pto(1).k = 0;                                  % PTO Stiffness Coeff [Nm/rad]
-pto(1).c = 0;                                  % PTO Damping Coeff [Nsm/rad]
-pto(1).loc = [0 0 -8.9];                       % PTO Global Location [m]
+pto(1) = ptoClass('PTO1');                      % Initialize ptoClass for PTO1
+pto(1).k = 0;                                   % PTO Stiffness Coeff [Nm/rad]
+pto(1).c = 0;                                   % PTO Damping Coeff [Nsm/rad]
+pto(1).loc = [0 0 -8.9];                        % PTO Global Location [m]
--- tutorials/RM3/wecSimInputFile.m
+++ tutorials/RM3/wecSimInputFile.m
@@ -1,35 +1,35 @@
 %% Simulation Data
-simu = simulationClass();               %Create the Simulation Variable
-simu.simMechanicsFile = 'RM3.slx';      %Location of Simulink Model File
-% simu.mode = 'normal';                 %Specify Simulation Mode ('normal','accelerator','rapid-accelerator')
-% simu.explorer='on';                   %Turn SimMechanics Explorer (on/off)
-% simu.startTime = 0;                   %Simulation Start Time [s]
-simu.endTime=400;                       %Simulation End bdcloseTime [s]
-simu.solver = 'ode4';                   %simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
-simu.dt = 0.1; 							%Simulation time-step [s]
-simu.rampT = 100;                       %Wave Ramp Time Length [s]
+simu = simulationClass();               % Initialize Simulation Class
+simu.simMechanicsFile = 'RM3.slx';      % Specify Simulink Model File
+simu.mode = 'normal';                   % Specify Simulation Mode ('normal','accelerator','rapid-accelerator')
+simu.explorer='on';                     % Turn SimMechanics Explorer (on/off)
+simu.startTime = 0;                     % Simulation Start Time [s]
+simu.endTime=400;                       % Simulation End bdcloseTime [s]
+simu.solver = 'ode4';                   % simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
+simu.dt = 0.1; 							% Simulation time-step [s]
+simu.rampT = 100;                       % Wave Ramp Time Length [s]
 
 %% Wave Information 
 %% noWaveCIC, no waves with radiation CIC  
-% waves = waveClass('noWaveCIC');       %Create the Wave Variable and Specify Type      
+% waves = waveClass('noWaveCIC');       % Initialize Wave Class and Specify Type  
 
 %% Regular Waves  
-waves = waveClass('regularCIC');        %Create the Wave Variable and Specify Type                               
-waves.H = 2.5;                          %Wave Height [m]
-waves.T = 8;                            %Wave Period [s]
+waves = waveClass('regular');           % Initialize Wave Class and Specify Type                                 
+waves.H = 2.5;                          % Wave Height [m]
+waves.T = 8;                            % Wave Period [s]
 
 %% Irregular Waves using PM Spectrum with Convolution Integral Calculation
-% waves = waveClass('irregular');       %Create the Wave Variable and Specify Type
-% waves.H = 2.5;                        %Significant Wave Height [m]
-% waves.T = 8;                          %Peak Period [s]
-% waves.spectrumType = 'PM';
+% waves = waveClass('irregular');         % Initialize Wave Class and Specify Type
+% waves.H = 2.5;                          % Significant Wave Height [m]
+% waves.T = 8;                            % Peak Period [s]
+% waves.spectrumType = 'PM';              % Specify Spectrum Type
 
 %% Irregular Waves using BS Spectrum with State Space Calculation
-% waves = waveClass('irregular');       %Create the Wave Variable and Specify Type
-% waves.H = 2.5;                        %Significant Wave Height [m]
-% waves.T = 8;                          %Peak Period [s]
-% waves.spectrumType = 'BS';
-% simu.ssCalc = 1;						%Control option to use state space model 
+% waves = waveClass('irregular');         % Initialize Wave Class and Specify Type
+% waves.H = 2.5;                          % Significant Wave Height [m]
+% waves.T = 8;                            % Peak Period [s]
+% waves.spectrumType = 'BS';              % Specify Spectrum Type
+% simu.ssCalc = 1;                        % Turn on State Space
 
 %% Irregular Waves using User-Defined Spectrum
 % waves = waveClass('irregularImport');  %Create the Wave Variable and Specify Type
@@ -44,25 +44,25 @@ waves.T = 8;                            %Wave Period [s]
 body(1) = bodyClass('hydroData/rm3.h5');      
     %Create the body(1) Variable, Set Location of Hydrodynamic Data File 
     %and Body Number Within this File.   
+body(1).geometryFile = 'geometry/float.stl';    %Location of Geomtry File
 body(1).mass = 'equilibrium';                   
     %Body Mass. The 'equilibrium' Option Sets it to the Displaced Water 
     %Weight.
 body(1).momOfInertia = [20907301 21306090.66 37085481.11];  %Moment of Inertia [kg*m^2]     
-body(1).geometryFile = 'geometry/float.stl';    %Location of Geomtry File
 
 %% Spar/Plate
 body(2) = bodyClass('hydroData/rm3.h5'); 
+body(2).geometryFile = 'geometry/plate.stl'; 
 body(2).mass = 'equilibrium';                   
 body(2).momOfInertia = [94419614.57 94407091.24 28542224.82];
-body(2).geometryFile = 'geometry/plate.stl'; 
 
 %% PTO and Constraint Parameters
 %% Floating (3DOF) Joint
-constraint(1) = constraintClass('Constraint1'); %Create Constraint Variable and Set Constraint Name
-constraint(1).loc = [0 0 0];                    %Constraint Location [m]
+constraint(1) = constraintClass('Constraint1'); % Initialize Constraint Class for Constraint1
+constraint(1).loc = [0 0 0];                    % Constraint Location [m]
 
 %% Translational PTO
-pto(1) = ptoClass('PTO1');                      %Create PTO Variable and Set PTO Name
-pto(1).k=0;                                     %PTO Stiffness [N/m]
-pto(1).c=1200000;                               %PTO Daming [N/(m/s)]
-pto(1).loc = [0 0 0];                           %PTO Location [m]
+pto(1) = ptoClass('PTO1');                      % Initialize PTO Class for PTO1
+pto(1).k=0;                                     % PTO Stiffness [N/m]
+pto(1).c=1200000;                               % PTO Daming [N/(m/s)]
+pto(1).loc = [0 0 0];                           % PTO Location [m]
