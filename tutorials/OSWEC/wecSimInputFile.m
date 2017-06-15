ab94706152aa7c24d4b80285facce05e1072b09b
Kelley Ruehl <kmruehl@sandia.gov>
2017-06-15 14:51:05 -0600

    cleaned up input file
    Former-commit-id: 1c5b6ccfe98bac90aa7dcc05c652c04e603bdd92

--- tutorials/OSWEC/wecSimInputFile.m
+++ tutorials/OSWEC/wecSimInputFile.m
@@ -11,43 +11,43 @@ simu.rampT = 100;                       % Wave Ramp Time Length [s]
 simu.CITime = 30;                       % Specify CI Time [s]
 
 %% Wave Information
-%% noWaveCIC, no waves with radiation CIC  
+% % noWaveCIC, no waves with radiation CIC  
 % waves = waveClass('noWaveCIC');       % Initialize Wave Class and Specify Type  
 
-%% Regular Waves 
+% % Regular Waves 
 waves = waveClass('regular');           % Initialize Wave Class and Specify Type                                 
 waves.H = 2.5;                          % Wave Height [m]
 waves.T = 8;                            % Wave Period [s]
 
-%% Irregular Waves using PM Spectrum with Convolution Integral Calculation
+% % Irregular Waves using PM Spectrum
 % waves = waveClass('irregular');         % Initialize Wave Class and Specify Type
 % waves.H = 2.5;                          % Significant Wave Height [m]
 % waves.T = 8;                            % Peak Period [s]
 % waves.spectrumType = 'PM';              % Specify Spectrum Type
 
-%% Irregular Waves using User-Defined Spectrum
+% % Irregular Waves using User-Defined Spectrum
 % waves = waveClass('irregularImport');
 % waves.spectrumDataFile = 'ndbcBuoyData.txt';
 
 %% Body Data
-%% Flap
+% Flap
 body(1) = bodyClass('hydroData/oswec.h5');      % Initialize bodyClass for Flap
+body(1).geometryFile = 'geometry/flap.stl';     % Geometry File
 body(1).mass = 127000;                          % User-Defined mass [kg]
 body(1).momOfInertia = [1.85e6 1.85e6 1.85e6];  % Moment of Inertia [kg-m^2]
-body(1).geometryFile = 'geometry/flap.stl';     % Geometry File
 
-%% Base
+% Base
 body(2) = bodyClass('hydroData/oswec.h5');      % Initialize bodyClass for Base
 body(2).geometryFile = 'geometry/base.stl';     % Geometry File
 body(2).mass = 'fixed';                         % Creates Fixed Body
 
 %% PTO and Constraint Parameters
-%% Fixed
+% Fixed
 constraint(1)= constraintClass('Constraint1');  % Initialize ConstraintClass for Constraint1
 constraint(1).loc = [0 0 -10];                  % Constraint Location [m]
 
-%% Rotational PTO
+% Rotational PTO
 pto(1) = ptoClass('PTO1');                      % Initialize ptoClass for PTO1
 pto(1).k = 0;                                   % PTO Stiffness Coeff [Nm/rad]
 pto(1).c = 0;                                   % PTO Damping Coeff [Nsm/rad]
-pto(1).loc = [0 0 -8.9];                        % PTO Global Location [m]
+pto(1).loc = [0 0 -8.9];                        % PTO Location [m]
--- tutorials/RM3/wecSimInputFile.m
+++ tutorials/RM3/wecSimInputFile.m
@@ -10,37 +10,37 @@ simu.dt = 0.1; 							% Simulation time-step [s]
 simu.rampT = 100;                       % Wave Ramp Time Length [s]
 
 %% Wave Information 
-%% noWaveCIC, no waves with radiation CIC  
+% % noWaveCIC, no waves with radiation CIC  
 % waves = waveClass('noWaveCIC');       % Initialize Wave Class and Specify Type  
 
-%% Regular Waves  
+% % Regular Waves  
 waves = waveClass('regular');           % Initialize Wave Class and Specify Type                                 
 waves.H = 2.5;                          % Wave Height [m]
 waves.T = 8;                            % Wave Period [s]
 
-%% Irregular Waves using PM Spectrum with Convolution Integral Calculation
+% % Irregular Waves using PM Spectrum 
 % waves = waveClass('irregular');         % Initialize Wave Class and Specify Type
 % waves.H = 2.5;                          % Significant Wave Height [m]
 % waves.T = 8;                            % Peak Period [s]
 % waves.spectrumType = 'PM';              % Specify Spectrum Type
 
-%% Irregular Waves using BS Spectrum with State Space Calculation
+% % Irregular Waves using BS Spectrum with State Space Calculation
 % waves = waveClass('irregular');         % Initialize Wave Class and Specify Type
 % waves.H = 2.5;                          % Significant Wave Height [m]
 % waves.T = 8;                            % Peak Period [s]
 % waves.spectrumType = 'BS';              % Specify Spectrum Type
 % simu.ssCalc = 1;                        % Turn on State Space
 
-%% Irregular Waves using User-Defined Spectrum
+% % Irregular Waves using User-Defined Spectrum
 % waves = waveClass('irregularImport');  %Create the Wave Variable and Specify Type
 % waves.spectrumDataFile = 'ndbcBuoyData.txt';  %Name of User-Defined Spectrum File [2,:] = [omega, Sf]
 
-%% User-Defined Time-Series
+% % User-Defined Time-Series
 % waves = waveClass('userDefined');     %Create the Wave Variable and Specify Type
 % waves.etaDataFile = 'umpqua46229_6_2008.mat'; % Name of User-Defined Time-Series File [:,2] = [time, wave_elev]
 
 %% Body Data
-%% Float
+% Float
 body(1) = bodyClass('hydroData/rm3.h5');      
     %Create the body(1) Variable, Set Location of Hydrodynamic Data File 
     %and Body Number Within this File.   
@@ -50,19 +50,19 @@ body(1).mass = 'equilibrium';
     %Weight.
 body(1).momOfInertia = [20907301 21306090.66 37085481.11];  %Moment of Inertia [kg*m^2]     
 
-%% Spar/Plate
+% Spar/Plate
 body(2) = bodyClass('hydroData/rm3.h5'); 
 body(2).geometryFile = 'geometry/plate.stl'; 
 body(2).mass = 'equilibrium';                   
 body(2).momOfInertia = [94419614.57 94407091.24 28542224.82];
 
 %% PTO and Constraint Parameters
-%% Floating (3DOF) Joint
+% Floating (3DOF) Joint
 constraint(1) = constraintClass('Constraint1'); % Initialize Constraint Class for Constraint1
 constraint(1).loc = [0 0 0];                    % Constraint Location [m]
 
-%% Translational PTO
+% Translational PTO
 pto(1) = ptoClass('PTO1');                      % Initialize PTO Class for PTO1
-pto(1).k=0;                                     % PTO Stiffness [N/m]
-pto(1).c=1200000;                               % PTO Daming [N/(m/s)]
+pto(1).k = 0;                                   % PTO Stiffness [N/m]
+pto(1).c = 1200000;                             % PTO Damping [N/(m/s)]
 pto(1).loc = [0 0 0];                           % PTO Location [m]
