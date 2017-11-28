6dcf6365f599136c9ba3ccbf19340ea75db7d63c
yuyihsiang <yi-hsiang.yu@nrel.gov>
2017-11-28 11:51:22 -0700

    update the verification cases

--- tutorials/verificationCases/runVerificationCases.m
+++ tutorials/verificationCases/runVerificationCases.m
@@ -1,12 +1,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Copyright 2014 the National Renewable Energy Laboratory and Sandia Corporation
-% 
+%
 % Licensed under the Apache License, Version 2.0 (the "License");
 % you may not use this file except in compliance with the License.
 % You may obtain a copy of the License at
-% 
+%
 %     http://www.apache.org/licenses/LICENSE-2.0
-% 
+%
 % Unless required by applicable law or agreed to in writing, software
 % distributed under the License is distributed on an "AS IS" BASIS,
 % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,12 +15,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % As specified by user input, runs regular and irregular WEC-Sim runs for
 % RM3, plots a comparison of simulation results vs. stored verification
-% runs, and plots solver-to-solver comparison. 
+% runs, and plots solver-to-solver comparison.
 
 %% User Input
-global plotNO; 
+global plotNO;
 runReg=1;       % 1 to run regular wave simulations
-runIrreg=1;     % 1 to run irregular wave simulations 
+runIrreg=1;     % 1 to run irregular wave simulations
 plotNO=1;       % 1 to plot new run vs. stored run for comparison of each solver
 plotSolvers=1;  % 1 to plot new run comparison by sln method
 openCompare=1;  % 1 opens all new run vs. stored run plots for comparison of each solver
@@ -29,32 +29,40 @@ openCompare=1;  % 1 opens all new run vs. stored run plots for comparison of eac
 if runReg==1
     cd RegularWaves/regular; runLoadRegular; cd .. ;
     savefig('figReg');
-    cd regularCIC; runLoadRegularCIC; cd .. ; 
-    savefig('figRegCIC'); 
-    cd regularSS; runLoadRegularSS; cd .. ; 
+    cd regularCIC; runLoadRegularCIC; cd .. ;
+    savefig('figRegCIC');
+    cd regularSS; runLoadRegularSS; cd .. ;
     savefig('figRegSS');
     cd .. ;  close all;
 end
 if runIrreg==1
-    cd IrregularWaves/irregularCIC; runLoadIrregularCIC; cd ..; 
-    savefig('figIrregCIC') ; 
-    cd irregularSS; runLoadIrregularSS; cd ..; 
+    cd IrregularWaves/irregularCIC; runLoadIrregularCIC; cd ..;
+    savefig('figIrregCIC') ;
+    cd irregularSS; runLoadIrregularSS; cd ..;
     savefig('figIrregSS');
-    cd .. ; close all;    
+    cd .. ; close all;
 end
 
 %% Plot Solver Comparisons
 if plotSolvers==1
-    cd RegularWaves; printPlotRegular;
-    cd .. ; 
-    cd IrregularWaves; printPlotIrregular;
-    cd ..
+    if runReg==1
+        cd RegularWaves; printPlotRegular;
+        cd .. ;
+    end
+    if runIrreg==1
+        cd IrregularWaves; printPlotIrregular;
+        cd ..
+    end
 end
 
 %% Open new vs. oig Comparisons
 if openCompare==1
-    cd RegularWaves; openfig('figReg.fig');openfig('figRegCIC.fig');openfig('figRegSS.fig');
-    cd .. ; 
-    cd IrregularWaves; openfig('figIrregCIC.fig');openfig('figIrregSS.fig');
-    cd ..
+    if runReg==1
+        cd RegularWaves; openfig('figReg.fig');openfig('figRegCIC.fig');openfig('figRegSS.fig');
+        cd .. ;
+    end
+    if runIrreg==1
+        cd IrregularWaves; openfig('figIrregCIC.fig');openfig('figIrregSS.fig');
+        cd ..
+    end
 end
