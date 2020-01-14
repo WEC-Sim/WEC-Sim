0e579ce3340400a98e0a020dfbaa0e7705315d83
dav-og <12831266+dav-og@users.noreply.github.com>
2020-01-14 14:45:31 -0700

    add local frame kinematics to postprocessing
    if simu.yawNonLin = 1 then 3 additional arrays are stored in output.bodies (positionLocal, velocityLocal, accelerationLocal). These arrays transform the global kinematic vectors around the z-axis to the body's local frame (effectively 'zeroing' the yaw angle displacement, which may be large when using yawNonLin).
    
    Some variable names updated in kinYawTransform function in WECSim_Lib.slx

--- tutorials/OSWEC/userDefinedFunctions.m
+++ tutorials/OSWEC/userDefinedFunctions.m
@@ -1,18 +1,18 @@
-%Example of user input MATLAB file for post processing
-close all
-
-%Plot waves
-waves.plotEta(simu.rampTime);
-try 
-    waves.plotSpectrum();
-catch
-end
-
-% Plot RY forces for body 1
-plotForces(output,1,5)
-
-%Plot RY response for body 1
-output.plotResponse(1,5);
-
-% Plot x forces for body 2
-plotForces(output,2,1)
+% %Example of user input MATLAB file for post processing
+% close all
+% 
+% %Plot waves
+% waves.plotEta(simu.rampTime);
+% try 
+%     waves.plotSpectrum();
+% catch
+% end
+% 
+% % Plot RY forces for body 1
+% plotForces(output,1,5)
+% 
+% %Plot RY response for body 1
+% output.plotResponse(1,5);
+% 
+% % Plot x forces for body 2
+% plotForces(output,2,1)
