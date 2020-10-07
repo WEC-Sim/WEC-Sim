cadaa82df0c151e0059fccd509bf7913e5d71aec
akeeste <72414466+akeeste@users.noreply.github.com>
2020-10-07 12:02:51 -0500

    Revert "tutorials"
    This reverts commit b14731fbe7b8ae6d5b9db40155456e9a9000c490.

--- source/wecSimStartup.m
+++ source/wecSimStartup.m
@@ -5,3 +5,4 @@
 wecSimPath = '<wecSim>';
 addpath(genpath(wecSimPath));
 
+% some arbitrary changes to test github
--- tutorials/RM3/wecSimInputFile.m
+++ tutorials/RM3/wecSimInputFile.m
@@ -1,6 +1,6 @@
 %% Simulation Data
 simu = simulationClass();               % Initialize simulationClass
-simu.simMechanicsFile = 'RM3.slx';      % Simulink Model File
+simu.simMechanicsFile = '.slx';         % Simulink Model File
 simu.startTime = 0;                     % Simulation Start Time [s]
 simu.rampTime = 100;                   	% Wave Ramp Time [s]
 simu.endTime=400;                       % Simulation End Time [s]
@@ -8,19 +8,19 @@ simu.dt = 0.1; 							% Simulation time-step [s]
 
 %% Wave Information 
 % Regular Waves  
-waves = waveClass('regular');           % Initialize waveClass
-waves.T = 2.5;                          % Wave Period [s]
-waves.H = 8;                            % Wave Height [m]
+waves = waveClass('type');              % Initialize waveClass
+waves.T = 999;                          % Wave Period [s]
+waves.H = 999;                          % Wave Height [m]
 
 %% Body Data
 % Float
-body(1) = bodyClass('hydroData/rm3.h5');        % Initialize bodyClass for Float      
+body(1) = bodyClass('hydroData/*.h5');          % Initialize bodyClass for Float      
 body(1).geometryFile = 'geometry/float.stl';    % Geomtry File
 body(1).mass = 'equilibrium';                   % Mass [kg]
 body(1).momOfInertia = [20907301 21306090.66 37085481.11];  % Moment of Inertia [kg*m^2]     
 
 % Spar/Plate
-body(2) = bodyClass('hydroData/rm3.h5');        % Initialize bodyClass for Spar/Plate
+body(2) = bodyClass('hydroData/*.h5');          % Initialize bodyClass for Spar/Plate
 body(2).geometryFile = 'geometry/plate.stl';    % Geometry File   
 body(2).mass = 'equilibrium';                   % Mass [kg]
 body(2).momOfInertia = [94419614.57 94407091.24 28542224.82];   % Moment of Inertia [kg*m^2]     
@@ -32,6 +32,6 @@ constraint(1).loc = [0 0 0];                    % Constraint Location [m]
 
 % Translational PTO
 pto(1) = ptoClass('PTO1');                      % Initialize ptoClass for PTO1
-pto(1).k = 0;                                   % PTO Stiffness [N/m]
-pto(1).c = 1200000;                             % PTO Damping [N/(m/s)]
+pto(1).k = 999;                                 % PTO Stiffness [N/m]
+pto(1).c = 999;                                 % PTO Damping [N/(m/s)]
 pto(1).loc = [0 0 0];                           % PTO Location [m]
