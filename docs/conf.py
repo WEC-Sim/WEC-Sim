fb9e7530e6c7ea59464a8a9ea9d5dfadbed62e32
Mathew Topper <damm_horse@yahoo.co.uk>
2021-01-22 12:06:29 +0000

    Fix whitelist rules

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ matlab_src_dir = os.path.abspath("../source")
 matlab_keep_package_prefix = False
 
 # sphinxcontrib-versioning settings
-scv_whitelist_branches = ('master', 'dev')
+scv_whitelist_branches = ('^master$', '^dev$', '^new_docs_structure$')
 scv_whitelist_tags = (re.compile('a^'),)
 
 # Add any paths that contain templates here, relative to this directory.
