d4a261408d5c0c0f142ac88d01f5ca36fdd1993d
salhus <84348506+salhus@users.noreply.github.com>
2021-06-21 18:13:35 -0400

    Installation/activation/deactivation using Startup and Cleanup scripts
    startup.m : This scripts initiates the WEC-Sim environment and adds all necessary sub-folders on the MATLAB path. This script also sets simulink to put all the extra slrpj files in a 'work' folder.
    cleanup.m : This script removes all the folders and sets the simulink back to default settings.
    An empty folder titles 'work' needs to be created so that the startup.m script runs without errors.
    The advantage of these two scripts is that it simplifies the installation process and the activation every time a user needs to use WEC-Sim, but then change the working folder to some other folder and do other work.
    
    A new user will just need to download the wecsim folder from GitHub and run the startup script and not go through setting up the paths and MATLAB startup directory manually.
    
    Cheers,
    Sal

---
+++ cleanup.m
@@ -0,0 +1,24 @@
+
+% First written by : Gordon G. Parker, Michigan Technological University
+% Modifed for WEC-Sim by: Sal Husain, sal.husain@nrel.gov
+% Last modified : June 9th, 2021
+
+% Root directory of this running .m file
+projectRootDir = fileparts(mfilename('fullpath'));
+
+% Remove project directories from path
+rmpath(fullfile(projectRootDir,genpath('examples')));
+rmpath(fullfile(projectRootDir,genpath('Sal')));
+rmpath(fullfile(projectRootDir,genpath('source')));
+rmpath(fullfile(projectRootDir,genpath('tests')));
+rmpath(fullfile(projectRootDir,genpath('tutorials')));
+% rmpath(fullfile(projectRootDir,genpath('models')));
+rmpath(fullfile(projectRootDir,'work'));
+% rmpath(fullfile(projectRootDir,genpath('scripts')));
+% rmpath(fullfile(projectRootDir,'code'));
+
+% Reset the loction of Simulink-generated files
+Simulink.fileGenControl('reset');
+
+% leave no trace...
+clear projectRootDir
