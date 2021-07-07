c182ef118a6d061ce063c966b340785a93f9e584
Mathew Topper <damm_horse@yahoo.co.uk>
2021-07-07 17:12:33 +0100

    Add static files in gh-pages to docs/_assets folder

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,8 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_include']
+exclude_patterns = ['_include',
+                    '_assets']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
