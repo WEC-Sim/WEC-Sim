f417b5439e9c5fa6e079b5b59be2b8f9ccea4973
salhus <shusain@nrel.gov>
2021-07-23 15:31:57 -0600

    update to create work
    This commit ensures the work folder exists, if not it will create it.

--- activate_wecsim.m
+++ activate_wecsim.m
@@ -1,8 +1,14 @@
 % Root directory of this running .m file
+warning off
 projectRootDir = pwd;
 % Add project directories to path
 addpath(fullfile(projectRootDir,genpath('source')),'-end');
 addpath(fullfile(projectRootDir,'work'),'-end');
+% Check if work folder exists, if it doesn't create it
+status = mkdir('work');
+if status == 0
+    mkdir 'work'
+end
 % Save Simulink-generated helper files to work
 Simulink.fileGenControl('set',...
     'CacheFolder',fullfile(projectRootDir,'work'))
