9ec5a5f6e3e8e34e99fa115d0dd644ff7d59c74a
jtgrasb <jtgrasb@sandia.gov>
2021-08-12 15:23:24 -0400

    Remove unnecessary changes

--- source/objects/waveClass.m
+++ source/objects/waveClass.m
@@ -182,44 +182,6 @@ classdef waveClass<handle
             ylabel('Spectrum (m^2-s/rad)');
         end
         
-        function plotSurfgif(obj,body_pos)
-            
-            
-            
-            h = figure;
-            axis tight manual % this ensures that getframe() returns a consistent size
-            filename = 'Surf.gif';
-            
-            random=[-.1,.1];
-           
-            for n = 1:1:length(obj.waveAmpTime(:,1))
-                
-                plot(random,obj.waveAmpTime(n,2)*ones(1,2), '-b');
-                hold on
-                plot(random,body_pos(n)*ones(1,2), '-r');
-                hold on
-                
-                legend('Wave Elevation','Body Displacement')
-                hold off
-                
-                axis([-.1 .1 -2 2])
-                pause(.01)
-             
-                  % Capture the plot as an image 
-                  frame = getframe(h); 
-                  im = frame2im(frame); 
-                  [imind,cm] = rgb2ind(im,256); 
-                  % Write to the GIF File 
-                  if n == 1 
-                      imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
-                  else 
-                      imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',.01); 
-                  end 
-                %}
-            end
-            %}
-        end
-        
         function waveSetup(obj,bemFreq,wDepth,rampTime,dt,maxIt,g, rho, endTime)
             % This method calculates WEC-Sim's wave properties 
             % based on the specified wave type.
