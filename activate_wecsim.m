6415bd1a413cb873073c8126b07677f53d9ce97d
salhus <shusain@nrel.gov>
2021-08-25 10:31:43 -0600

    activate_deactivate_gettingstarted
    Cleaned up PR of the previous activate deactivate PR

---
+++ activate_wecsim.m
@@ -0,0 +1,13 @@
+% Root directory of this running .m file
+projectRootDir = pwd;
+% Add project directories to path
+addpath(fullfile(projectRootDir,genpath('source')),'-end');
+addpath(fullfile(projectRootDir,'work'),'-end');
+% Check if work folder exists, if it doesn't create it
+status = mkdir('work');
+if status == 0
+    mkdir 'work'
+end
+% Save Simulink-generated helper files to work
+Simulink.fileGenControl('set',...
+    'CacheFolder',fullfile(projectRootDir,'work'))
