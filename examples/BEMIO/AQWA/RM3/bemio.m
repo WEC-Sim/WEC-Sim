afb09e62e1403884507fe81eb7c1fe7c747ea1c6
jtgrasb <jtgrasb@sandia.gov>
2021-08-30 12:59:59 -0400

    Fixed Read_AQWA

--- examples/BEMIO/AQWA/RM3/bemio.m
+++ examples/BEMIO/AQWA/RM3/bemio.m
@@ -2,7 +2,7 @@ clc; clear all; close all;
 hydro = struct();
 
 hydro = Read_AQWA(hydro, 'RM3.AH1', 'RM3.LIS');
-hydro = Radiation_IRF(hydro,20,[],[],[],[]);
+hydro = Radiation_IRF(hydro,60,[],[],[],[]); %extend the time period
 hydro = Radiation_IRF_SS(hydro,[],[]);
 hydro = Excitation_IRF(hydro,20,[],[],[],[]);
 Write_H5(hydro)
