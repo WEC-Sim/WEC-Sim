7172a53361cc2f8923f04c87aef1189d19e50235
jtgrasb <jtgrasb@sandia.gov>
2021-12-06 10:43:16 -0500

    Remove tests

--- examples/Single_body_tests/hydroData/bemio.m
+++
@@ -1,8 +0,0 @@
-hydro = struct();
-
-hydro = Read_WAMIT(hydro,'rm3.out',[]);
-hydro = Radiation_IRF(hydro,60,[],[],[],[]);
-hydro = Radiation_IRF_SS(hydro,[],[]);
-hydro = Excitation_IRF(hydro,157,[],[],[],[]);
-Write_H5(hydro)
-Plot_BEMIO(hydro)
