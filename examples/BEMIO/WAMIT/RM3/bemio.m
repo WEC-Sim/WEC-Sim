f29cd094caa376f8e33c6e33759ca8a4d12a4f15
dav-og <12831266+dav-og@users.noreply.github.com>
2022-02-23 05:35:13 -0700

    vectorization of radiationIRF() function
    hat tip to @crobarcro for originally implementing this improvement in RenewNet
    
    trapz function used in B(w) cosine transformation now applied to whole arrays rather than iteratively within the k'th loop (ie over IRF time)
    
    initial tests with RM3 show 60% reduction in RIRF computation time

--- examples/BEMIO/WAMIT/RM3/bemio.m
+++ examples/BEMIO/WAMIT/RM3/bemio.m
@@ -1,8 +1,11 @@
 hydro = struct();
 
 hydro = readWAMIT(hydro,'rm3.out',[]);
+tic
 hydro = radiationIRF(hydro,20,[],[],[],[]);
-hydro = radiationIRFSS(hydro,[],[]);
+toc
+%hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,20,[],[],[],[]);
 writeBEMIOH5(hydro)
-plotBEMIO(hydro)
+
+%plotBEMIO(hydro)
