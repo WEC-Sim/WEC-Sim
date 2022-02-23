7a81ac7dcb584933a779f3ed8ad091a48cae1e33
David Ogden <12831266+dav-og@users.noreply.github.com>
2022-02-23 05:56:59 -0700

    reverting rm3 bemio.m - removing profiling

--- examples/BEMIO/WAMIT/RM3/bemio.m
+++ examples/BEMIO/WAMIT/RM3/bemio.m
@@ -1,11 +1,8 @@
 hydro = struct();
 
 hydro = readWAMIT(hydro,'rm3.out',[]);
-tic
 hydro = radiationIRF(hydro,20,[],[],[],[]);
-toc
-%hydro = radiationIRFSS(hydro,[],[]);
+hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,20,[],[],[],[]);
 writeBEMIOH5(hydro)
-
-%plotBEMIO(hydro)
+plotBEMIO(hydro)
