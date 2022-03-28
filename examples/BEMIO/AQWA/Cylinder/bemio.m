7832c1aa8e536729d048dd3c31f34fa1bfd99f7e
jtgrasb <87095491+jtgrasb@users.noreply.github.com>
2022-03-28 18:43:05 -0400

    Fix AQWA data for sphere, ellipsoid, and WEC3 (#797)
    * Fix AQWA data for sphere, ellipsoid, and WEC3
    
    * Fix Sphere and add BEM type to plot legends
    
    * Undo BEM Plot Legends
    
    * Improve RM3 example
    
    * Limit IRF Frequency
    
    * updating bemio files
    
    Co-authored-by: kmruehl <kmruehl@sandia.gov>

--- examples/BEMIO/AQWA/Cylinder/bemio.m
+++ examples/BEMIO/AQWA/Cylinder/bemio.m
@@ -1,8 +1,12 @@
-hydro = struct();
+% clc; clear all; close all;
 
+%% hydro data
+hydro = struct();
 hydro = readAQWA(hydro, 'cylinder.AH1', 'cylinder.LIS');
 hydro = radiationIRF(hydro,5,[],[],[],[]);
 hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,5,[],[],[],[]);
 writeBEMIOH5(hydro)
-plotBEMIO(hydro)
+
+%% Plot hydro data
+% plotBEMIO(hydro)
