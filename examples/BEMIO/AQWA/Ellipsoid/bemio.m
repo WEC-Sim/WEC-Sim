e98dbe0dd3235c18d2f33d1e553fe3097c002b00
kmruehl <kmruehl@sandia.gov>
2022-03-28 16:42:40 -0600

    updating bemio files

--- examples/BEMIO/AQWA/Ellipsoid/bemio.m
+++ examples/BEMIO/AQWA/Ellipsoid/bemio.m
@@ -1,9 +1,12 @@
-hydro = struct();
+% clc; clear all; close all;
 
+%% hydro data
+hydro = struct();
 hydro = readAQWA(hydro, 'ellipsoid.AH1', 'ellipsoid.LIS');
 hydro = radiationIRF(hydro,10,[],[],[],[]);
 hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,15,[],[],[],[]);
 writeBEMIOH5(hydro)
-plotBEMIO(hydro)
 
+%% Plot hydro data
+% plotBEMIO(hydro)
--- examples/BEMIO/AQWA/RM3/bemio.m
+++ examples/BEMIO/AQWA/RM3/bemio.m
@@ -1,8 +1,12 @@
-hydro = struct();
+% clc; clear all; close all;
 
+%% hydro data
+hydro = struct();
 hydro = readAQWA(hydro, 'RM3.AH1', 'RM3.LIS');
 hydro = radiationIRF(hydro,150,[],[],[],1.8);
 hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,150,[],[],[],1.8);
 writeBEMIOH5(hydro)
-plotBEMIO(hydro)
+
+%% Plot hydro data
+% plotBEMIO(hydro)
