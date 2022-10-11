d3a6d3cbafcbd6f74abc5b7a199f3c6aa4b9a3a0
Husain <shusain@nrel.gov>
2022-10-11 14:57:41 -0600

    update_maske_extraction_function

--- source/functions/simulink/mask/ExtractMaskVariables.m
+++ source/functions/simulink/mask/ExtractMaskVariables.m
@@ -1,12 +1,18 @@
 function mask =  ExtractMaskVariables(MaskNamewithAddress)
 
-%Refer https://www.mathworks.com/help/simulink/slref/simulink.mask-class.html
-%Refer https://www.mathworks.com/help/simulink/ug/control-masks-programmatically.html
+% Refer https://www.mathworks.com/help/simulink/slref/simulink.mask-class.html
+% Refer https://www.mathworks.com/help/simulink/ug/control-masks-programmatically.html
+% Open the WEC-Sim model where the mask to be extracted resides.
+% In the argument enter the address for the mask including the Simulink
+% model name. For example: mask =  ExtractMaskVariables('RM3/Float')
 sys = MaskNamewithAddress;
 load_system(sys)
 mask.BlockPaths = find_system(sys,'Type','Block');
-mask.handle = getSimulinkBlockHandle(BlockPaths{3});
-mask.ObjectParameters = get_param(handle,'ObjectParameters');
-mask.DialogParameters = get_param(handle,'DialogParameters');
-mask.p = Simulink.Mask.get(handle);
-mask.MaskVars = p.getWorkspaceVariables;
+mask.handle = getSimulinkBlockHandle(mask.BlockPaths);
+mask.ObjectParameters = get_param(mask.handle,'ObjectParameters');
+try
+mask.DialogParameters = get_param(mask.handle,'mask.DialogParameters');
+catch
+end
+mask.p = Simulink.Mask.get(mask.handle);
+mask.MaskVars = mask.p.getWorkspaceVariables;
