d6e839e79e8e65a0fb55e0720ff4b972263979f5
David Ogden <david.ogden.nrel@outlook.com>
2022-11-02 11:36:34 +0000

    included readCAPYTAINE() argument to explicitly define KH.dat & Hydrostatics.dat locations

--- examples/BEMIO/CAPYTAINE/ellipsoid/bemio.m
+++ examples/BEMIO/CAPYTAINE/ellipsoid/bemio.m
@@ -1,6 +1,6 @@
 hydro = struct();
 
-hydro = readCAPYTAINE(hydro,'ellipsoid_full.nc');
+hydro = readCAPYTAINE(hydro,'ellipsoid_full.nc',[]);
 hydro = radiationIRF(hydro,15,[],[],[],[]);
 hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,15,[],[],[],[]);
