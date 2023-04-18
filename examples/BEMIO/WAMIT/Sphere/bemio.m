e828c93d4dcc3294ade1cd156ca3aab54e8312c1
jtgrasb <jtgrasb@sandia.gov>
2023-04-18 16:31:20 -0400

    Remove testing scripts

--- examples/BEMIO/WAMIT/Sphere/bemio.m
+++ examples/BEMIO/WAMIT/Sphere/bemio.m
@@ -4,5 +4,5 @@ hydro = readWAMIT(hydro,'sphere.out',[]);
 hydro = radiationIRF(hydro,15,[],[],[],[]);
 hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,15,[],[],[],[]);
-% writeBEMIOH5(hydro)
-% plotBEMIO(hydro)
+writeBEMIOH5(hydro)
+plotBEMIO(hydro)
