d115ff4caa928b4769e37bd407ab9ea2cfce488c
Husain <shusain@nrel.gov>
2023-06-02 12:00:02 -0600

    Update readWAMIT.m
    remove comment for cleaner code

--- source/functions/BEMIO/readWAMIT.m
+++ source/functions/BEMIO/readWAMIT.m
@@ -92,7 +92,6 @@ for n = 1:N
         hydro(F).Khs(4:5,4,hydro(F).Nb) = tmp{1}(1:2);
         tmp = textscan(raw{n+3}(find(raw{n+3}==':')+1:end),'%f');
         hydro(F).Khs(5,5:6,hydro(F).Nb) = tmp{1};
-        % 6th row must be zeros
     end
     if isempty(strfind(raw{n},'Wave period'))==0
         if isempty(strfind(raw{n},'Wave period = infinite'))==0  T = 0;  end
