8f4186f39d36f0e970a7b088c77de955bd3b4642
Ruehl <kmruehl@sandia.gov>
2024-09-04 17:54:23 -0600

    reverting input file changes

--- examples/RM3/wecSimInputFile.m
+++ examples/RM3/wecSimInputFile.m
@@ -4,8 +4,8 @@ simu.simMechanicsFile = 'RM3.slx';      % Specify Simulink Model File
 simu.mode = 'normal';                   % Specify Simulation Mode ('normal','accelerator','rapid-accelerator')
 simu.explorer = 'on';                   % Turn SimMechanics Explorer (on/off)
 simu.startTime = 0;                     % Simulation Start Time [s]
-simu.rampTime = 60;                    % Wave Ramp Time [s]
-simu.endTime = 100;                     % Simulation End Time [s]
+simu.rampTime = 100;                    % Wave Ramp Time [s]
+simu.endTime = 400;                     % Simulation End Time [s]
 simu.solver = 'ode4';                   % simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
 simu.dt = 0.1; 							% Simulation time-step [s]
 
