d2bf6e4a05abf3432f56a1b70ae43ade36d54ac6
RyanDavies19 <ryandavies2019@gmail.com>
2024-12-05 09:47:24 -0700

    Adjusting variables to camel case

--- source/objects/responseClass.m
+++ source/objects/responseClass.m
@@ -321,7 +321,7 @@ classdef responseClass<handle
             end
         end
         
-        function obj = loadMoorDyn(obj,linesNum, InputFile)            
+        function obj = loadMoorDyn(obj,linesNum, inputFile)            
             % This method reads MoorDyn outputs for each instance of the
             % ``mooringClass``
             %            
@@ -329,25 +329,25 @@ classdef responseClass<handle
             % ------------
             %     linesNum : integer
             %         the number of MoorDyn lines
-            %     InputFile : text
+            %     inputFile : text
             %         the infile text name 
             %
             
             arguments
                 obj
                 linesNum (1,1) double {mustBeInteger}
-                InputFile (1,:) {mustBeText}
+                inputFile (1,:) {mustBeText}
             end
             
             % load out files. First find the path and rootname. 
-            InputStrings = strsplit(InputFile, '.');
+            inputStrings = strsplit(inputFile, '.');
 
-            if length(InputStrings)> 2
+            if length(inputStrings)> 2
                 error('MoorDyn input file path and root cannot contain "." characters') % this is already checked when mooring class is set up but double checking again
             end
             
-            filerootpath = char(InputStrings(1));
-            filename = append(filerootpath,'.out');
+            fileRootPath = char(inputStrings(1));
+            filename = append(fileRootPath,'.out');
             fid = fopen(filename, 'r');
             header = strsplit(fgetl(fid));
             data = dlmread(filename,'',1,0);
@@ -360,7 +360,7 @@ classdef responseClass<handle
             % load Line#.out
             for iline=1:linesNum
                 eval(['obj.moorDyn.Line' num2str(iline) '=struct();']);
-                filename = [append(filerootpath,'_Line') num2str(iline) '.out'];
+                filename = [append(fileRootPath,'_Line') num2str(iline) '.out'];
                 try
                     fid = fopen(filename);
                     header = strsplit(strtrim(fgetl(fid)));
