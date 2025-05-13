21bfcfb2746f018ea45bee8d9d76bb1024c3036d
Kelley Ruehl <kmruehl@sandia.gov>
2025-05-13 14:57:54 -0600

    ParaView feature for multiple mooring connections #1440 (#1455)
    * fix paraview output for multiple bodies
    
    * Add post-processing for multiple MoorDyn connections
    
    * fix paraview macro for multiple moordyn connections
    
    * update docs
    
    * Update mooring.rst
    
    ---------
    
    Co-authored-by: jtgrasb <87095491+jtgrasb@users.noreply.github.com>

--- source/functions/paraview/writeParaviewMooring.m
+++ source/functions/paraview/writeParaviewMooring.m
@@ -1,4 +1,4 @@
-function writeParaviewMooring(moorDyn, model,t,simdate,nline,nnode,paraviewPath,TimeBodyParav,NewTimeParaview)
+function writeParaviewMooring(moorDyn, model,t,simdate,nline,nnode,paraviewPath,TimeBodyParav,NewTimeParaview,iMoor)
 % Method to write ``vtp`` Paraview visualization files for the
 % mooringClass. Executed by paraviewVisualization.m when simu.paraview.option=1 
 % in the wecSimInputFile.m
@@ -23,6 +23,8 @@ function writeParaviewMooring(moorDyn, model,t,simdate,nline,nnode,paraviewPath,
 %       Paraview time vector
 %   NewTimeParaview : float vector
 %       Total simulation time interpolated by the Paraview time step
+%   iMoor : int
+%       MoorDyn index
 %
 
 nsegment = nnode -1;
@@ -40,7 +42,7 @@ for iline = 1:nline
 end
 for it = 1:length(TimeBodyParav)
     % open file
-    filename = [paraviewPath, filesep 'mooring' filesep 'mooring_' num2str(it) '.vtp'];
+    filename = [paraviewPath, filesep 'mooring' num2str(iMoor) filesep 'mooring_' num2str(it) '.vtp'];
     fid = fopen(filename, 'w');
     % write header
     fprintf(fid, '<?xml version="1.0"?>\n');
