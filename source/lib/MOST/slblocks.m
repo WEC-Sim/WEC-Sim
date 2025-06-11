d2ae2d7f3e0611d85d8c3b2a59e5dd2d1c79c973
akeeste <akeeste@sandia.gov>
2025-06-11 15:45:31 -0500

    minor formatting for source/lib/**/slBlocks.m

--- source/lib/MOST/slblocks.m
+++ source/lib/MOST/slblocks.m
@@ -1,8 +1,8 @@
 function blkStruct = slblocks
-% This function specifies that the library 'mylib'
+% This function specifies that the library 'MOST_Lib'
 % should appear in the Library Browser with the 
-% name 'My Library'
+% name 'MOST'
 
-    Browser.Library = 'MOST_Lib';
-    Browser.Name = 'MOST';
-    blkStruct.Browser = Browser;
+Browser.Library = 'MOST_Lib';
+Browser.Name = 'MOST';
+blkStruct.Browser = Browser;
