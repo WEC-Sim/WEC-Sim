da7a42baec262f3e59c02afbefd43617318d10ee
Mohamed Shabara <84589678+MShabara@users.noreply.github.com>
2025-11-19 14:17:02 -0700

    Update AQWA file paths for hydro data

--- examples/BEMIO/AQWA/Cylinder/bemio.m
+++ examples/BEMIO/AQWA/Cylinder/bemio.m
@@ -2,7 +2,7 @@
 
 %% hydro data
 hydro = struct();
-hydro = readAQWA(hydro, 'ANALYSIS.AH1', 'ANALYSIS.LIS');
+hydro = readAQWA(hydro, 'cylinder.AH1', 'cylinder.LIS');
 hydro = radiationIRF(hydro,5,[],[],[],[]);
 hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,5,[],[],[],[]);
