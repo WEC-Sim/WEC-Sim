4a113faa72457a26be9e7d8355a5fe7c8302264d
akeeste <akeeste@sandia.gov>
2026-06-30 10:38:00 -0500

    revert changes to example files

--- examples/RM3/wecSimInputFile.m
+++ examples/RM3/wecSimInputFile.m
@@ -8,15 +8,13 @@ simu.rampTime = 100;                    % Wave Ramp Time [s]
 simu.endTime = 400;                     % Simulation End Time [s]
 simu.solver = 'ode4';                   % simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
 simu.dt = 0.1; 							% Simulation time-step [s]
-simu.stateSpace = 1;
-simu.b2b = 0;
 
 %% Wave Information 
 % % noWaveCIC, no waves with radiation CIC  
 % waves = waveClass('noWaveCIC');       % Initialize Wave Class and Specify Type  
 
 % % Regular Waves  
-waves = waveClass('regularCIC');           % Initialize Wave Class and Specify Type                                 
+waves = waveClass('regular');           % Initialize Wave Class and Specify Type                                 
 waves.height = 2.5;                     % Wave Height [m]
 waves.period = 8;                       % Wave Period [s]
 
@@ -68,18 +66,18 @@ body(1).mass = 'equilibrium';
 body(1).inertia = [20907301 21306090.66 37085481.11];  % Moment of Inertia [kg*m^2]     
 
 % Spar/Plate
-% body(2) = bodyClass('hydroData/rm3.h5'); 
-% body(2).geometryFile = 'geometry/plate.stl'; 
-% body(2).mass = 'equilibrium';                   
-% body(2).inertia = [94419614.57 94407091.24 28542224.82];
+body(2) = bodyClass('hydroData/rm3.h5'); 
+body(2).geometryFile = 'geometry/plate.stl'; 
+body(2).mass = 'equilibrium';                   
+body(2).inertia = [94419614.57 94407091.24 28542224.82];
 
 %% PTO and Constraint Parameters
 % Floating (3DOF) Joint
 constraint(1) = constraintClass('Constraint1'); % Initialize Constraint Class for Constraint1
 constraint(1).location = [0 0 0];               % Constraint Location [m]
 
-% % Translational PTO
-% pto(1) = ptoClass('PTO1');                      % Initialize PTO Class for PTO1
-% pto(1).stiffness = 0;                           % PTO Stiffness [N/m]
-% pto(1).damping = 1200000;                       % PTO Damping [N/(m/s)]
-% pto(1).location = [0 0 0];                      % PTO Location [m]
+% Translational PTO
+pto(1) = ptoClass('PTO1');                      % Initialize PTO Class for PTO1
+pto(1).stiffness = 0;                           % PTO Stiffness [N/m]
+pto(1).damping = 1200000;                       % PTO Damping [N/(m/s)]
+pto(1).location = [0 0 0];                      % PTO Location [m]
